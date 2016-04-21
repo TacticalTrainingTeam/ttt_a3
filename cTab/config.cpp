d47a25da6979e67e0d3ce977bf4cbe874112bd7c
TheConen <theconen@gmail.com>
2016-04-22 00:18:23 +0200

    removed auto tracking of vehicles from cTab

--- cTab/config.cpp
+++ cTab/config.cpp
@@ -68,13 +68,13 @@ class CfgMods {
 #include "\cTab\CfgFunctions.h"
 class cTab_settings {
 	// define vehicle classes that are equipped with FBCB2 devices
-	cTab_vehicleClass_has_FBCB2[] = {"MRAP_01_base_F","MRAP_02_base_F","MRAP_03_base_F","Wheeled_APC_F","Tank","Truck_01_base_F","Truck_03_base_F"};
+	cTab_vehicleClass_has_FBCB2[] = {};
 
 	// define vehicle classes that are equipped with TAD devices
-	cTab_vehicleClass_has_TAD[] = {"Helicopter","Plane"};
+	cTab_vehicleClass_has_TAD[] = {};
 
 	// define helmet classes that are equipped with a helmet cam
-	cTab_helmetClass_has_HCam[] = {"H_HelmetB_light","H_Helmet_Kerry","H_HelmetSpecB","H_HelmetO_ocamo","BWA3_OpsCore_Fleck_Camera","BWA3_OpsCore_Schwarz_Camera","BWA3_OpsCore_Tropen_Camera"};
+	cTab_helmetClass_has_HCam[] = {};
 };
 
 // define the default key setup; can be altered in-game using CBA's keybinding system
