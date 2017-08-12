9c459382c83f5f41fb6d9fadca79afe33d311557
Stura7 <game.stubi@outlook.com>
2017-08-12 14:19:45 +0000

    PMC vehicles added

---
+++ ttt_vehicles_pmc/config.cpp
@@ -0,0 +1,40 @@
+#include "\ttt_a3\ttt_vehicles_pmc\macros.hpp"
+
+class CfgPatches {
+	class ttt_vehicles_pmc {
+		requiredAddons[] = {"ttt_main"};		
+		units[] = {	"TTT_IMV_MG_PMC",
+					"TTT_IMV_TOW_PMC", 
+					"TTT_IMV_AA_PMC",
+					"TTT_IFV_PMC",
+					"TTT_LUV_LOG_PMC",					
+					"TTT_Heli_Heavy_PMC",
+					"TTT_Heli_Medium_PMC",
+					"TTT_Heli_Light_PMC",
+					"TTT_CAS_PMC"
+		};
+		weapons[] = {};
+	};
+};
+
+class CfgVehicleClasses {
+	class TTT_Vehicles_PMC {
+		displayName = "$STR_TTT_Vehilce_PMC";	//In-game name
+		priority = 24; 							// How far down it is on the menu
+	};
+};
+
+class CfgVehicles {
+	// External class references
+	class B_Plane_CAS_01_dynamicLoadout_F;
+	class B_T_VTOL_01_vehicle_F;
+    class CUP_B_HMMWV_Avenger_USMC;
+    class CUP_B_HMMWV_TOW_USMC;
+    class rhsusf_m1025_w_s_m2;
+    class rhsusf_m998_w_s2dr_fulltop;
+	class rhsusf_M1117_W;
+	class RHS_MELB_MH6M;
+	class UK3CB_BAF_Merlin_HC3_24;
+
+	#include "\ttt_a3\ttt_vehicles_pmc\vehicles.hpp"
+};
---
+++ ttt_vehicles_pmc/vehicles.hpp
@@ -0,0 +1,230 @@
+/*
+#############################################################
+### TTT Truppfahrzeug
+#############################################################
+*/
+
+class TTT_IMV_MG_PMC : rhsusf_m1025_w_s_m2 {
+    author = "$STR_Tactical_Training_Team";
+    scope = 2;
+    faction = "TTT_faction_PMC";
+    vehicleClass = "TTT_Vehicles_PMC";
+    crew = "B_Soldier_F";
+    displayName = "$STR_TTT_IMV_MG_PMC";
+    typicalCargo[] = {};
+
+    class TransportWeapons {
+    };
+    class TransportItems {
+		item_xx(ToolKit,1);
+    };
+    class TransportMagazines {
+    };
+    class TransportBackpacks {
+    };
+};
+
+
+class TTT_IMV_TOW_PMC : CUP_B_HMMWV_TOW_USMC {
+    author = "$STR_Tactical_Training_Team";
+    scope = 2;
+    faction = "TTT_faction_PMC";
+    vehicleClass = "TTT_Vehicles_PMC";
+    crew = "B_Soldier_F";
+    displayName = "$STR_TTT_IMV_TOW_PMC";
+    typicalCargo[] = {};
+
+    class TransportWeapons {
+    };
+    class TransportItems {
+		item_xx(ToolKit,1);
+    };
+    class TransportMagazines {
+    };
+    class TransportBackpacks {
+    };
+};
+
+class TTT_IMV_AA_PMC : CUP_B_HMMWV_Avenger_USMC {
+    author = "$STR_Tactical_Training_Team";
+    scope = 2;
+    faction = "TTT_faction_PMC";
+    vehicleClass = "TTT_Vehicles_PMC";
+    crew = "B_Soldier_F";
+    displayName = "$STR_TTT_IMV_AA_PMC";
+    typicalCargo[] = {};
+
+    class TransportWeapons {
+    };
+    class TransportItems {
+		item_xx(ToolKit,1);
+    };
+    class TransportMagazines {
+    };
+    class TransportBackpacks {
+    };
+};
+
+
+/*
+#############################################################
+### TTT Transporter
+#############################################################
+*/
+
+class TTT_LUV_LOG_PMC : rhsusf_m998_w_s2dr_fulltop {
+    author = "$STR_Tactical_Training_Team";
+    scope = 2;
+    faction = "TTT_faction_PMC";
+    vehicleClass = "TTT_Vehicles_PMC";
+    crew = "B_Soldier_F";
+    displayName = "$STR_TTT_LUV_LOG_PMC";
+    typicalCargo[] = {};
+    ace_cargo_space = 9;
+
+    class TransportWeapons {
+    };
+    class TransportItems {
+		item_xx(ToolKit,1);
+    };
+    class TransportMagazines {
+    };
+    class TransportBackpacks {
+    };
+};
+
+/*
+#############################################################
+### TTT Schuetzenpanzer
+#############################################################
+*/
+
+class TTT_IFV_PMC : rhsusf_M1117_W {
+    author = "$STR_Tactical_Training_Team";
+    scope = 2;
+    faction = "TTT_faction_PMC";
+    vehicleClass = "TTT_Vehicles_PMC";
+    crew = "B_Soldier_F";
+    displayName = "$STR_TTT_IFV_PMC";
+    typicalCargo[] = {};
+    class TransportWeapons {
+    };
+    class TransportItems {
+		item_xx(ToolKit,1);
+    };
+    class TransportMagazines {
+    };
+    class TransportBackpacks {
+    };
+};
+
+
+/*
+#############################################################
+### TTT Transporthelikopter (schwer)
+#############################################################
+*/
+
+class TTT_Heli_Heavy_PMC : B_T_VTOL_01_vehicle_F {
+    author = "$STR_Tactical_Training_Team";
+    scope = 2;
+    faction = "TTT_faction_PMC";
+    vehicleClass = "TTT_Vehicles_PMC";
+    crew = "B_Helipilot_F";
+    displayName = "$STR_TTT_Heli_Heavy_PMC";
+    typicalCargo[] = {};
+    ace_cargo_space = 60;
+
+    class TransportWeapons {
+    };
+    class TransportItems {
+		item_xx(ToolKit,1);
+    };
+    class TransportMagazines {
+    };
+    class TransportBackpacks {
+		//backpack_xx(B_Parachute,30);
+    };
+};
+
+
+/*
+#############################################################
+### TTT Transporthelikopter (medium)
+#############################################################
+*/
+
+class TTT_Heli_Medium_PMC : UK3CB_BAF_Merlin_HC3_24 {
+    author = "$STR_Tactical_Training_Team";
+    scope = 2;
+    faction = "TTT_faction_PMC";
+    vehicleClass = "TTT_Vehicles_PMC";
+    crew = "B_Helipilot_F";
+    displayName = "$STR_TTT_Heli_Medium_PMC";
+    typicalCargo[] = {};
+
+    class TransportWeapons {
+    };
+    class TransportItems {
+		item_xx(ToolKit,1);
+    };
+    class TransportMagazines {
+    };
+    class TransportBackpacks {
+    };
+};
+
+/*
+#############################################################
+### TTT Transporthelikopter (leicht)
+#############################################################
+*/
+
+class TTT_Heli_Light_PMC : RHS_MELB_MH6M {
+    author = "$STR_Tactical_Training_Team";
+    scope = 2;
+    faction = "TTT_faction_PMC";
+    vehicleClass = "TTT_Vehicles_PMC";
+    crew = "B_Helipilot_F";
+    displayName = "$STR_TTT_Heli_Light_PMC";
+    typicalCargo[] = {};
+    ace_cargo_space = 4;
+
+    class TransportWeapons {
+    };
+    class TransportItems {
+		item_xx(ToolKit,1);
+    };
+    class TransportMagazines {
+    };
+    class TransportBackpacks {
+    };
+};
+
+/*
+#############################################################
+### TTT Combat Aircraft Close Air Support
+#############################################################
+*/
+
+    class TTT_CAS_PMC : B_Plane_CAS_01_dynamicLoadout_F {
+        author = "$STR_Tactical_Training_Team";
+        scope = 2;
+        scopeCurator = 2;
+        faction = "TTT_faction_BW";
+        vehicleClass = "TTT_Vehicles_BW";
+        displayName = "$STR_TTT_CAS_MC";
+        crew = "TTT_Platinum_1_US_Tropentarn";
+        typicalCargo[] = {};
+
+		class TransportWeapons {
+		};
+		class TransportItems {
+			item_xx(ToolKit,1);
+		};
+		class TransportMagazines {
+		};
+		class TransportBackpacks {
+			backpack_xx(B_Parachute,1);
+		};		
+    };
