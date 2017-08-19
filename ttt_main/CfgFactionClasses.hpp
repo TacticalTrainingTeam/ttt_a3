683dfe3dda75d29ac52701f71491f9b4e4c7be94
Stura <game.stubi@outlook.com>
2017-08-19 15:09:34 +0200

    added stringtable.xml added new Subfaction

--- ttt_main/CfgFactionClasses.hpp
+++ ttt_main/CfgFactionClasses.hpp
@@ -11,7 +11,13 @@ class CfgFactionClasses {
 	class TTT_faction_Us {
 		icon = "\ttt_a3\ttt_main\icons\TTT_faction_icon.paa";
 		displayName = "TTT (US)";
-		priority = 1;
+		priority = 2;
 		side = 1;
 	};
+	class TTT_faction_PMC {
+		icon = "\ttt_a3\ttt_main\icons\TTT_faction_icon.paa";
+		displayName = "TTT (PMC)";
+		priority = 3;
+		side = 1;
+	};	
 };
