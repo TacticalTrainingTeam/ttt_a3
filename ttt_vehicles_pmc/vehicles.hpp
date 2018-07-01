807ab1898fe7134282c0a6a6fca322ad9e3a36d2
specop0 <specop0@yahoo.de>
2018-07-01 14:20:18 +0200

    hide PMC vehicles

--- ttt_vehicles_pmc/vehicles.hpp
+++ ttt_vehicles_pmc/vehicles.hpp
@@ -6,7 +6,7 @@
 
 class TTT_IMV_MG_PMC : rhsusf_m1025_w_s_m2 {
     author = "$STR_Tactical_Training_Team";
-    scope = 2;
+    scope = 0;
     faction = "TTT_faction_PMC";
     vehicleClass = "TTT_Vehicles_PMC";
     crew = "B_Soldier_F";
@@ -27,7 +27,7 @@ class TTT_IMV_MG_PMC : rhsusf_m1025_w_s_m2 {
 
 class TTT_IMV_TOW_PMC : CUP_B_HMMWV_TOW_USMC {
     author = "$STR_Tactical_Training_Team";
-    scope = 2;
+    scope = 0;
     faction = "TTT_faction_PMC";
     vehicleClass = "TTT_Vehicles_PMC";
     crew = "B_Soldier_F";
@@ -47,7 +47,7 @@ class TTT_IMV_TOW_PMC : CUP_B_HMMWV_TOW_USMC {
 
 class TTT_IMV_AA_PMC : CUP_B_HMMWV_Avenger_USMC {
     author = "$STR_Tactical_Training_Team";
-    scope = 2;
+    scope = 0;
     faction = "TTT_faction_PMC";
 	editorSubcategory = "EdSubcat_Cars";
     vehicleClass = "TTT_Vehicles_PMC";
@@ -75,7 +75,7 @@ class TTT_IMV_AA_PMC : CUP_B_HMMWV_Avenger_USMC {
 
 class TTT_LUV_LOG_PMC : rhsusf_m998_w_s_2dr_fulltop {
     author = "$STR_Tactical_Training_Team";
-    scope = 2;
+    scope = 0;
     faction = "TTT_faction_PMC";
     vehicleClass = "TTT_Vehicles_PMC";
     crew = "B_Soldier_F";
@@ -102,7 +102,7 @@ class TTT_LUV_LOG_PMC : rhsusf_m998_w_s_2dr_fulltop {
 
 class TTT_IFV_PMC : rhsusf_M1117_W {
     author = "$STR_Tactical_Training_Team";
-    scope = 2;
+    scope = 0;
     faction = "TTT_faction_PMC";
 	editorSubcategory = "EdSubcat_APCs";
     vehicleClass = "TTT_Vehicles_PMC";
@@ -129,7 +129,7 @@ class TTT_IFV_PMC : rhsusf_M1117_W {
 
 class TTT_MTA_Heavy_PMC : B_T_VTOL_01_vehicle_F {
     author = "$STR_Tactical_Training_Team";
-    scope = 2;
+    scope = 0;
     faction = "TTT_faction_PMC";
 	editorSubcategory = "EdSubcat_Helicopters";
     vehicleClass = "TTT_Vehicles_PMC";
@@ -159,7 +159,7 @@ class TTT_MTA_Heavy_PMC : B_T_VTOL_01_vehicle_F {
 
 class TTT_MTA_Medium_PMC : UK3CB_BAF_Merlin_HC3_24 {
     author = "$STR_Tactical_Training_Team";
-    scope = 2;
+    scope = 0;
     faction = "TTT_faction_PMC";
     vehicleClass = "TTT_Vehicles_PMC";
     crew = "B_Helipilot_F";
@@ -185,7 +185,7 @@ class TTT_MTA_Medium_PMC : UK3CB_BAF_Merlin_HC3_24 {
 
 class TTT_MTA_Light_PMC : RHS_MELB_MH6M {
     author = "$STR_Tactical_Training_Team";
-    scope = 2;
+    scope = 0;
     faction = "TTT_faction_PMC";
     vehicleClass = "TTT_Vehicles_PMC";
     crew = "B_Helipilot_F";
@@ -212,8 +212,7 @@ class TTT_MTA_Light_PMC : RHS_MELB_MH6M {
 
     class TTT_MRCA_PMC : B_Plane_CAS_01_dynamicLoadout_F {
         author = "$STR_Tactical_Training_Team";
-        scope = 2;
-        scopeCurator = 2;
+        scope = 0;
         faction = "TTT_faction_PMC";
         vehicleClass = "TTT_Vehicles_PMC";
         displayName = "$STR_TTT_MRCA_PMC";
