d425dafffb15c9afdac98d622bfa0cfb6e42f92a
Reimchen <Reimold95@web.de>
2018-07-13 00:38:44 +0200

    add Helmets 7+8
    + Helmets 7+8 - Flecktarn
    ~ Vererbung der Helme

--- ttt_units_bw/weapons_flecktarn.hpp
+++ ttt_units_bw/weapons_flecktarn.hpp
@@ -104,32 +104,49 @@
 	### Helme
 	#############################################################
 	*/
-	class TTT_Helmet_1_BW_Flecktarn : TTT_Helmet_1_BW_Tropentarn {
+	class TTT_Helmet_1_BW_Flecktarn : TTT_Helmet_Base {
+		author = "$STR_Tactical_Training_Team";
+		scope = 2;
 		displayName = "TTT Helm I (BW Flecktarn)";
 		hiddenSelectionsTextures[] = {"\ttt_a3\ttt_units_bw\data\TTT_helmet_1_bw_flecktarn.paa"};
 	};
 	
-	class TTT_Helmet_2_BW_Flecktarn : TTT_Helmet_2_BW_Tropentarn {
+	class TTT_Helmet_2_BW_Flecktarn : TTT_Helmet_1_BW_Flecktarn {
+		author = "$STR_Tactical_Training_Team";
 		displayName = "TTT Helm II (BW Flecktarn)";
 		hiddenSelectionsTextures[] = {"\ttt_a3\ttt_units_bw\data\TTT_helmet_2_bw_flecktarn.paa"};
 	};
 	
-	class TTT_Helmet_3_BW_Flecktarn : TTT_Helmet_3_BW_Tropentarn {
+	class TTT_Helmet_3_BW_Flecktarn : TTT_Helmet_1_BW_Flecktarn {
+		author = "$STR_Tactical_Training_Team";
 		displayName = "TTT Helm III (BW Flecktarn)";
 		hiddenSelectionsTextures[] = {"\ttt_a3\ttt_units_bw\data\TTT_helmet_3_bw_flecktarn.paa"};
 	};
 	
-	class TTT_Helmet_4_BW_Flecktarn : TTT_Helmet_4_BW_Tropentarn {
+	class TTT_Helmet_4_BW_Flecktarn : TTT_Helmet_1_BW_Flecktarn {
+		author = "$STR_Tactical_Training_Team";
 		displayName = "TTT Helm IV (BW Flecktarn)";
 		hiddenSelectionsTextures[] = {"\ttt_a3\ttt_units_bw\data\TTT_helmet_4_bw_flecktarn.paa"};
 	};
 	
-	class TTT_Helmet_5_BW_Flecktarn : TTT_Helmet_5_BW_Tropentarn {
+	class TTT_Helmet_5_BW_Flecktarn : TTT_Helmet_1_BW_Flecktarn {
+		author = "$STR_Tactical_Training_Team";
 		displayName = "TTT Helm V (BW Flecktarn)";
 		hiddenSelectionsTextures[] = {"\ttt_a3\ttt_units_bw\data\TTT_helmet_5_bw_flecktarn.paa"};
 	};
 	
-	class TTT_Helmet_6_BW_Flecktarn : TTT_Helmet_6_BW_Tropentarn {
+	class TTT_Helmet_6_BW_Flecktarn : TTT_Helmet_1_BW_Flecktarn {
+		author = "$STR_Tactical_Training_Team";
 		displayName = "TTT Helm VI (BW Flecktarn)";
 		hiddenSelectionsTextures[] = {"\ttt_a3\ttt_units_bw\data\TTT_helmet_6_bw_flecktarn.paa"};
+	};
+	class TTT_Helmet_7_BW_Flecktarn : TTT_Helmet_1_BW_Flecktarn {
+		author = "$STR_Tactical_Training_Team";
+		displayName = "TTT Helm VII (BW Flecktarn)";
+		hiddenSelectionsTextures[] = {"\ttt_a3\ttt_units_bw\data\TTT_helmet_7_bw_flecktarn.paa"};
+	};
+	class TTT_Helmet_8_BW_Flecktarn : TTT_Helmet_1_BW_Flecktarn {
+		author = "$STR_Tactical_Training_Team";
+		displayName = "TTT Helm VIII (BW Flecktarn)";
+		hiddenSelectionsTextures[] = {"\ttt_a3\ttt_units_bw\data\TTT_helmet_8_bw_flecktarn.paa"};
 	};
