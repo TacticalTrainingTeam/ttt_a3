8e47c6dc0325ad797dcbd0b4741fad58a585198b
Andx <andx667@protonmail.com>
2024-10-27 00:09:36 +0200

    added baretts

---
+++ addons/ai/CfgFunctions.hpp
@@ -0,0 +1,11 @@
+class CfgFunctions {
+	class fix_ai {
+		tag = "fix_ai";
+		class init {
+			class fix_ai_init {
+				file = "ttt_ai\fix_ai.sqf";
+				postInit = 1;
+			};
+		};		
+	};
+};
---
+++ addons/compat_rhs/CfgWeapons.hpp
@@ -0,0 +1,7 @@
+//https://ace3.acemod.org/wiki/framework/wirecutter-framework
+//https://ace3.acemod.org/wiki/framework/trenches-frameworkclass CfgWeapons
+
+class CfgWeapons
+{
+
+};
