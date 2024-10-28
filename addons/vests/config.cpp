e7add575b83d1dd7a7569c295cc5961a49e9fda5
Andx <andx667@protonmail.com>
2024-10-28 16:09:51 +0100

    added last readme

--- addons/vests/config.cpp
+++ addons/vests/config.cpp
@@ -40,8 +40,8 @@ class CfgPatches {
 			"TTT_Vest_Heavy_Gold_US_Desert",
 			"TTT_Vest_Lite_Gold_US_Desert",
 			"TTT_Vest_Heavy_Grey_US_Desert",
-			"TTT_Vest_Lite_Grey_US_Desert",		
-			"TTT_Vest_Heavy_Orange_US_Desert",			
+			"TTT_Vest_Lite_Grey_US_Desert",
+			"TTT_Vest_Heavy_Orange_US_Desert",
 			"TTT_Vest_Lite_Orange_US_Desert",
 		};
 
