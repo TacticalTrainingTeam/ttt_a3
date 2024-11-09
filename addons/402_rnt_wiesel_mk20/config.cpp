100e399724fd9b24bdb23cbb9382db3db167ffe9
Andx <andx667@protonmail.com>
2024-11-09 02:10:47 +0100

    clean up

--- addons/402_rnt_wiesel_mk20/config.cpp
+++ addons/402_rnt_wiesel_mk20/config.cpp
@@ -5,7 +5,7 @@ class CfgPatches {
         // Meta information for editor
         name = COMPONENT_NAME;
         author = ECSTRING(402_common,author);
-        authors[] = {"Mishkar"}; //Last Guy made Advanced SLingloading
+        authors[] = {"Mishkar"}; 
         url = ECSTRING(main,URL);
 
         // Minimum compatible version. When the game's version is lower, pop-up warning will appear when launching the game. Note: was disabled on purpose some time late into Arma 2: OA.
