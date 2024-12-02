d6eef8ae9b90ca2943d02549e999bc84b48a8cb9
Andx667 <andx667@protonmail.com>
2024-12-02 08:52:35 +0100

    initial commit

---
+++ addons/insignias/CfgUnitInsignia.hpp
@@ -0,0 +1,11 @@
+class CfgUnitInsignia
+{
+	class TTT
+	{
+		displayName = "111th Infantry Division";								// Name displayed in Arsenal
+		author = "Bohemia Interactive";											// Author displayed in Arsenal
+		texture = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\111thID_ca.paa";			// Image path
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";	// .rvmat path
+		textureVehicle = "";													// Does nothing, reserved for future use
+	};
+};
---
+++ addons/insignias/readme.md
@@ -0,0 +1,5 @@
+# Insignien
+
+## Maintainer
+
+- Andx
