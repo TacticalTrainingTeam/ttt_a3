5a36ef07b474328df087ef275780e5f8d13f0400
Andx <andx667@protonmail.com>
2024-12-14 10:13:18 +0100

    helmets

--- addons/helmets/CfgVehicles.hpp
+++ addons/helmets/CfgVehicles.hpp
@@ -5,6 +5,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_1_bw_flecktarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_fleck_1_preview.jpg);
         scope = 2;
         
         class TransportItems {
@@ -19,6 +20,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_2_bw_flecktarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_fleck_2_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_2_BW_Flecktarn {
@@ -32,6 +34,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_3_bw_flecktarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_fleck_3_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_3_BW_Flecktarn {
@@ -45,6 +48,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_4_bw_flecktarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_fleck_4_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_4_BW_Flecktarn {
@@ -58,6 +62,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_5_bw_flecktarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_fleck_5_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_5_BW_Flecktarn {
@@ -71,6 +76,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_6_bw_flecktarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_fleck_6_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_6_BW_Flecktarn {
@@ -84,6 +90,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_7_bw_flecktarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_fleck_7_preview.jpg);
         
         class TransportItems {
             class ttt_Helmet_7_BW_Flecktarn {
@@ -97,6 +104,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_8_bw_flecktarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_fleck_8_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_8_BW_Flecktarn {
@@ -110,6 +118,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_9_bw_flecktarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_fleck_9_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_9_BW_Flecktarn {
@@ -123,6 +132,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_10_bw_flecktarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_fleck_10_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_10_BW_Flecktarn {
@@ -136,6 +146,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_11_bw_flecktarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_fleck_11_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_11_BW_Flecktarn {
@@ -149,6 +160,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_1_bw_tropentarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_tropen_1_preview.jpg);
         scope = 2;
         
         class TransportItems {
@@ -163,6 +175,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_2_bw_tropentarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_tropen_2_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_2_BW_tropentarn {
@@ -176,6 +189,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_3_bw_tropentarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_tropen_3_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_3_BW_tropentarn {
@@ -189,6 +203,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_4_bw_tropentarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_tropen_4_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_4_BW_tropentarn {
@@ -202,6 +217,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_5_bw_tropentarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_tropen_5_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_5_BW_tropentarn {
@@ -215,6 +231,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_6_bw_tropentarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_tropen_6_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_6_BW_tropentarn {
@@ -228,6 +245,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_7_bw_tropentarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_tropen_7_preview.jpg);
         
         class TransportItems {
             class ttt_Helmet_7_BW_tropentarn {
@@ -241,6 +259,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_8_bw_tropentarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_tropen_8_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_8_BW_tropentarn {
@@ -254,6 +273,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_9_bw_tropentarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_tropen_9_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_9_BW_tropentarn {
@@ -267,6 +287,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_10_bw_tropentarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_tropen_10_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_10_BW_tropentarn {
@@ -280,6 +301,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_11_bw_tropentarn_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_bw_tropen_11_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_11_BW_tropentarn {
@@ -293,6 +315,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_1_us_desert_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_us_desert_1_preview.jpg);
         scope = 2;
         
         class TransportItems {
@@ -307,6 +330,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_2_us_desert_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_us_desert_2_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_2_us_desert {
@@ -320,6 +344,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_3_us_desert_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_us_desert_3_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_3_us_desert {
@@ -333,6 +358,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_4_us_desert_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_us_desert_4_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_4_us_desert {
@@ -346,6 +372,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_5_us_desert_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_us_desert_5_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_5_us_desert {
@@ -359,6 +386,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_6_us_desert_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_us_desert_6_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_6_us_desert {
@@ -372,6 +400,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_7_us_desert_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_us_desert_7_preview.jpg);
         
         class TransportItems {
             class ttt_Helmet_7_us_desert {
@@ -385,6 +414,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_8_us_desert_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_us_desert_8_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_8_us_desert {
@@ -398,6 +428,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_9_us_desert_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_us_desert_9_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_9_us_desert {
@@ -411,6 +442,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_10_us_desert_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_us_desert_10_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_10_us_desert {
@@ -424,6 +456,7 @@ class CfgVehicles {
         author = ECSTRING(main,TacticalTrainingTeam);
         authors[] = {"Andx"};
         displayName = CSTRING(helmet_11_us_desert_displayName);
+        editorPreview = QPATHTOF(data\previews\ttt_Helmet_Item_us_desert_11_preview.jpg);
 
         class TransportItems {
             class ttt_Helmet_11_us_desert {
