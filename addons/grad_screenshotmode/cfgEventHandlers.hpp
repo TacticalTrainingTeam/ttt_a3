12f6d17ff43e008a93519872dc1205abb14c856d
Andx <andx667@protonmail.com>
2025-01-03 14:25:56 +0100

    removed help text for now

--- addons/grad_screenshotmode/cfgEventHandlers.hpp
+++ addons/grad_screenshotmode/cfgEventHandlers.hpp
@@ -8,18 +8,10 @@ class Extended_PreInit_EventHandlers {
     class ADDON {
         init = QUOTE(call COMPILE_SCRIPT(XEH_preInit));
     };
-
-    class GVAR(clientPreInit) {
-        clientInit = QUOTE(call FUNC(clientPreInit));
-    };
 };
 
 class Extended_PostInit_EventHandlers {
     class ADDON {
         init = QUOTE(call COMPILE_SCRIPT(XEH_postInit));
-    };
-
-    class GVAR(clientPostInit) {
-        clientInit = QUOTE(call FUNC(clientPostInit));
     };
 };
--- addons/grad_screenshotmode/functions/fnc_clientPreInit.sqf
+++
@@ -1,5 +0,0 @@
-#include "script_component.hpp"
-
-private _title = LSTRING(displayName);
-private _helpText = LSTRING(helpTexts);
-[_title,_helpText] call FUNC(addHelpRecord);
