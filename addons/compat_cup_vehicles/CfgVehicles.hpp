34ff2a0592c9bcdd5c63f1d3146cd54cdb742087
Andx667 <andx667@protonmail.com>
2025-01-13 20:35:47 +0100

    add viewports to bradley and btr type, smokelauncher for boxer gunner, improve handling of boxer fix fv mortarcarrier, fix bacjseat of m1038

--- addons/compat_cup_vehicles/CfgVehicles.hpp
+++ addons/compat_cup_vehicles/CfgVehicles.hpp
@@ -15,7 +15,7 @@ class CfgVehicles {
     class CUP_nHMMWV_Base: Car_F {
         class CargoTurret;
     };
-    
+
     class CUP_nM1037sc_Base: CUP_nHMMWV_Base {
         class AcreRacks {
             class Rack_1 {
@@ -99,7 +99,8 @@ class CfgVehicles {
     };
 
     // all below made by: AChesheireCat and PabstMirror
-    //source: https://github.com/BourbonWarfare/POTATO/blob/master/addons/miscFixes/patchCUP/config.cpp
+    // source: https://github.com/BourbonWarfare/POTATO/blob/master/addons/miscFixes/patchCUP/config.cpp
+
     // Fix broken artillery computer on FV432 Mortar (shows artillery computer for 7.62mg)
     class CUP_B_FV432_Bulldog_GB_D;
     class CUP_B_FV432_Base: CUP_B_FV432_Bulldog_GB_D {
@@ -155,7 +156,7 @@ class CfgVehicles {
         class CargoTurret;
         class Turrets {
             class MainTurret: NewTurret {
-                weapons[] = {"CUP_Vhmg_M3P_veh","SmokeLauncher"}; // was CUP_Vhmg_M2_veh
+                weapons[] = {"CUP_Vhmg_M2_veh","SmokeLauncher"};
                 magazines[] = {"CUP_200Rnd_TE1_Red_Tracer_127x99_M", "CUP_200Rnd_TE1_Red_Tracer_127x99_M", "CUP_200Rnd_TE1_Red_Tracer_127x99_M", "CUP_200Rnd_TE1_Red_Tracer_127x99_M", "CUP_200Rnd_TE1_Red_Tracer_127x99_M", "CUP_200Rnd_TE1_Red_Tracer_127x99_M", "SmokeLauncherMag"}; // was CUP_200Rnd_TE1_Red_Tracer_127x99_M
             };
 
@@ -176,20 +177,20 @@ class CfgVehicles {
             };
         };
 
-        class AnimationSources: AnimationSources {
-            class main_gun_muzzle_rot {
-                weapon = "CUP_Vhmg_M3P_veh";
-            };
-            class main_gun_reload {
-                weapon="CUP_Vhmg_M3P_veh";
-            };
-            class main_gun_reload_mag {
-                weapon="CUP_Vhmg_M3P_veh";
-            };
-            class main_gun_revolving {
-                weapon="CUP_Vhmg_M3P_veh";
-            };
-        };
+        // class AnimationSources: AnimationSources {
+        //     class main_gun_muzzle_rot {
+        //         weapon = "CUP_Vhmg_M3P_veh";
+        //     };
+        //     class main_gun_reload {
+        //         weapon="CUP_Vhmg_M3P_veh";
+        //     };
+        //     class main_gun_reload_mag {
+        //         weapon="CUP_Vhmg_M3P_veh";
+        //     };
+        //     class main_gun_revolving {
+        //         weapon="CUP_Vhmg_M3P_veh";
+        //     };
+        // };
     };
 
     class CUP_Boxer_Base_GMG: CUP_Boxer_Base_HMG {
@@ -202,6 +203,136 @@ class CfgVehicles {
             class CommanderTurret: NewTurret {
                 weapons[] = {}; // was "SmokeLauncher"
                 magazines[] = {}; // was "SmokeLauncherMag"
+            };
+        };
+    };
+
+    // CUP-ACE_Viewports compat
+    class CUP_BTR80_Common_Base: Wheeled_APC_F {
+        class ace_viewports {
+            class view_0 {
+                camLocation[] = {0.734863, 0.143927, -0.291199};
+                camAttach = 45;
+                compartments[]={"Compartment1"};
+                roles[]={"cargo"};
+            };
+            class view_1 {
+                camLocation[] = {0.950684, -0.00122738, -0.344391};
+                camAttach = 90;
+                compartments[]={"Compartment1"};
+                roles[]={"cargo"};
+            };
+            class view_2 {
+                camLocation[] = {0.932129, -0.729811, -0.319};
+                camAttach = 90;
+                compartments[]={"Compartment1"};
+                roles[]={"cargo"};
+            };
+            class view_3 {
+                camLocation[] = {-0.963379, -0.749183, -0.351501};
+                camAttach = -90;
+                compartments[]={"Compartment1"};
+                roles[]={"cargo"};
+            };
+            class view_4 {
+                camLocation[] = {-0.973145, 0.0148516, -0.364868};
+                camAttach = -90;
+                compartments[]={"Compartment1"};
+                roles[]={"cargo"};
+            };
+            class view_5 {
+                camLocation[] = {-0.73584, 0.119176, -0.291229};
+                camAttach = -45;
+                compartments[]={"Compartment1"};
+                roles[]={"cargo"};
+            };
+        };
+    };
+    class CUP_BTR90_Base: Wheeled_APC_F {
+        class ace_viewports {
+            class view_0 {
+                camLocation[] = {0.533417, 2.4613, -0.309951};
+                camAttach = 0;
+                compartments[]={"Compartment1"};
+                roles[]={"cargo"};
+            };
+            class view_1 {
+                camLocation[] = {0.877106, 2.38684, -0.306068};
+                camAttach = 30;
+                compartments[]={"Compartment1"};
+                roles[]={"cargo"};
+            };
+            class view_2 {
+                camLocation[] = {1.04312, 0.0909424, -0.281996};
+                camAttach = 60;
+                compartments[]={"Compartment1"};
+                roles[]={"cargo"};
+            };
+            class view_3 {
+                type = "";
+                camLocation[] = {1.03799, -0.928223, -0.282196};
+                camAttach = 80;
+                compartments[]={"Compartment1"};
+                roles[]={"cargo"};
+            };
+            class view_4 {
+                type = "";
+                camLocation[] = {-0.90097, -0.913086, -0.282192};
+                camAttach = -80;
+                compartments[]={"Compartment1"};
+                roles[]={"cargo"};
+            };
+            class view_5 {
+                type = "";
+                camLocation[] = {-0.939301, 0.109985, -0.281992};
+                camAttach = -60;
+                compartments[]={"Compartment1"};
+                roles[]={"cargo"};
+            };
+        };
+    };
+    class CUP_BTR90_HQ_Base: CUP_BTR90_Base { delete ace_viewports; }; // no cargo seats
+
+    class CUP_M2Bradley_Base: Tank_F {
+        ace_hunterkiller = 1;
+        class ace_viewports {
+            class view_0 {
+                camLocation[] = {0.987915, -0.324707, -0.0673385};
+                camAttach = 70;
+                roles[]={"cargo"};
+            };
+            class view_1 {
+                camLocation[] = {0.90979, -0.970215, -0.043139};
+                camAttach = 120;
+                roles[]={"cargo"};
+            };
+            class view_2 {
+                camLocation[] = {0.328003, -1.87402, -0.0408039};
+                camAttach = 160;
+                roles[]={"cargo"};
+            };
+            class view_3 {
+                camLocation[] = {-0.357178, -1.89063, -0.0480237};
+                camAttach = 175;
+                roles[]={"cargo"};
+            };
+            class view_4 {
+                camLocation[] = {-0.752075, -1.87061, -0.0522318};
+                camAttach = 190;
+                roles[]={"cargo"};
+            };
+            class view_5 {
+                camLocation[] = {-0.936401, -0.0942383, -0.107764};
+                camAttach = -75;
+                roles[]={"cargo"};
+            };
+            class SLD { // suppose to only be only on A3, but the A2s have the screen on the model?
+                screenLocation[] = {0.729126,-0.191597,-0.573349};
+                maxDistance = 5;
+                type = "screen";
+                camLocation[] = {0,0,0.05};
+                camAttach[] = {0,0};
+                roles[]={"cargo"};
             };
         };
     };
--- addons/compat_cup_vehicles/config.cpp
+++ addons/compat_cup_vehicles/config.cpp
@@ -12,7 +12,7 @@ class CfgPatches {
         requiredVersion = REQUIRED_VERSION;
         // Required addons, used for setting load order. (CfgPatches classname NOT PBO filename!)
         // When any of the addons are missing, a pop-up warning will appear when launching the game.
-        requiredAddons[] = {"cba_main", "CUP_Vehicles_LoadOrder", "ace_interaction"};
+        requiredAddons[] = {"cba_main", "CUP_Vehicles_LoadOrder", "ace_interaction", "ace_viewports"};
         // List of objects (CfgVehicles classes) contained in the addon. Important also for Zeus content (units and groups) unlocking.
         units[] = {};
         // List of weapons (CfgWeapons classes) contained in the addon.
