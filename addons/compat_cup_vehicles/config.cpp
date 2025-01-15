04dc4078bb5349e30dbb5656e0998f70e66d0c54
Andy Börner <andy.boerner@s2009.tu-chemnitz.de>
2025-01-15 11:05:27 +0100

    update authors and required addons

--- addons/compat_cup_vehicles/config.cpp
+++ addons/compat_cup_vehicles/config.cpp
@@ -5,14 +5,14 @@ class CfgPatches {
         // Meta information for editor
         name = COMPONENT_NAME;
         author = ECSTRING(main,TacticalTrainingTeam);
-        authors[] = {"Andx"};
+        authors[] = {"PabstMirror", "AChesheireCat", "Andx"};
         url = ECSTRING(main,URL);
 
         // Minimum compatible version. When the game's version is lower, pop-up warning will appear when launching the game. Note: was disabled on purpose some time late into Arma 2: OA.
         requiredVersion = REQUIRED_VERSION;
         // Required addons, used for setting load order. (CfgPatches classname NOT PBO filename!)
         // When any of the addons are missing, a pop-up warning will appear when launching the game.
-        requiredAddons[] = {"cba_main", "CUP_Vehicles_LoadOrder", "ace_interaction", "ace_viewports"};
+        requiredAddons[] = {"ttt_main", "CUP_Vehicles_LoadOrder", "ace_interaction", "ace_viewports"};
         // List of objects (CfgVehicles classes) contained in the addon. Important also for Zeus content (units and groups) unlocking.
         units[] = {};
         // List of weapons (CfgWeapons classes) contained in the addon.
