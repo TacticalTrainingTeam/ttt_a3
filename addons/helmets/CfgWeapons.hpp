01f16638dd9074148a86effc0209b53944950a94
Andx <andx667@protonmail.com>
2025-01-20 21:13:32 +0100

    update author

--- addons/helmets/CfgWeapons.hpp
+++ addons/helmets/CfgWeapons.hpp
@@ -75,7 +75,7 @@ class CfgWeapons {
     class H_HelmetB_plain_mcamo;
     class H_HelmetSpecB: H_HelmetB_plain_mcamo { class ItemInfo; };
     class ttt_Helmet_Base: H_HelmetSpecB {
-        author = QUOTE(Tactical Training Team);
+        author = ECSTRING(main,TacticalTrainingTeam);
         scope = 0;
         model = QUOTE(\A3\Characters_F\BLUFOR\headgear_b_helmet_light.p3d);
 
