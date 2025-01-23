545597cfd7444d979f961affe19cb50a70902c46
Andx <andx667@protonmail.com>
2025-01-23 18:40:39 +0100

    lowercase folder

---
+++ addons/402_smokelauncher/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT 402_smokelauncher
+#define COMPONENT_BEAUTIFIED PzGrenBtl 402 Smoke Launcher
+#include "\z\ttt\addons\main\script_mod.hpp"
+
+#include "\z\ttt\addons\main\script_macros.hpp"
