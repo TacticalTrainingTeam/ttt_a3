d283413db4c4a2211cbce0083cacb0070f308781
Andx <andx667@protonmail.com>
2025-01-24 23:41:46 +0100

    fix inheritance

--- addons/compat_xeh_fix/CfgVehicles.hpp
+++ addons/compat_xeh_fix/CfgVehicles.hpp
@@ -1,8 +1,6 @@
 class CfgVehicles {
     class ReammoBox_F;
     class PBW_Uniform1_Chest: ReammoBox_F { XEH_ENABLED; }; // "[PBW] Uniform Chest" @Bundeswehr_Kleiderkammer_PBW
-
-    class PBW_Uniform1_Chest;
     class PBW_Headgear_Chest: PBW_Uniform1_Chest { XEH_ENABLED; }; // "[PBW] Headgear Chest" @Bundeswehr_Kleiderkammer_PBW
 
     class C_man_pilot_F;
@@ -41,11 +39,8 @@ class CfgVehicles {
     class ACE_Explosives_Place_SLAM;
     class tsp_frameCharge_place: ACE_Explosives_Place_SLAM { XEH_ENABLED; }; // "Frame Charge" @Breach
     class tsp_popperCharge_place: ACE_Explosives_Place_SLAM { XEH_ENABLED; }; // "Lock Popper" @Breach
-    class tsp_popperCharge_place;
     class tsp_popperCharge_auto_place: tsp_popperCharge_place { XEH_ENABLED; }; // "Lock Popper (Auto-Fuse)" @Breach
     class tsp_stickCharge_place: ACE_Explosives_Place_SLAM { XEH_ENABLED; }; // "Stick Charge" @Breach
-
-    class tsp_stickCharge_place;
     class tsp_stickCharge_auto_place: tsp_stickCharge_place { XEH_ENABLED; }; // "Stick Charge (Auto-Fuse)" @Breach
 };
 
