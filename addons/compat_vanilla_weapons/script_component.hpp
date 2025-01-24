dce7cff425ed69dfe51fc9f6c95fb764097b8b7e
Andx <andx667@protonmail.com>
2025-01-24 09:21:13 +0100

    fix error

--- addons/compat_vanilla_weapons/script_component.hpp
+++ addons/compat_vanilla_weapons/script_component.hpp
@@ -1,5 +1,5 @@
-#define COMPONENT compat_vanilla_scopes
-#define COMPONENT_BEAUTIFIED Comapt Vanilla Scopes
+#define COMPONENT compat_vanilla_weapons
+#define COMPONENT_BEAUTIFIED Comapt Vanilla Weapons
 #include "\z\ttt\addons\main\script_mod.hpp"
 
 #include "\z\ttt\addons\main\script_macros.hpp"
