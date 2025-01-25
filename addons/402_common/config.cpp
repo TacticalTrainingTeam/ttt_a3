0be22a970a6765216e6f4d4527a84cdc0a3bf90a
Andx <andx667@protonmail.com>
2025-01-25 18:58:01 +0100

    fix string

--- addons/402_common/config.cpp
+++ addons/402_common/config.cpp
@@ -16,7 +16,7 @@ class CfgPatches {
             "acre_sys_intercom",
             "acre_sys_rack"
         };
-        author = ESTRING(402_common,author);
+        author = ECSTRING(402_common,author);
         authors[] = {"Timi007", "Mishkar"};
         url = CSTRING(URL);
         VERSION_CONFIG;
