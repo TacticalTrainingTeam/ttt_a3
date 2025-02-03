676bd4318fd903d998fffa20a728b2aa1b5e615a
Andx <andx667@protonmail.com>
2025-02-04 00:36:05 +0100

    fix subaddons

--- addons/compat_xeh_fix/compat_xeh_fix_breach/config.cpp
+++ addons/compat_xeh_fix/compat_xeh_fix_breach/config.cpp
@@ -1,18 +1,15 @@
 #include "script_component.hpp"
 
 class CfgPatches {
-    class ADDON {
+    class SUBADDON {
+        addonRootClass = QUOTE(ADDON);
         name = COMPONENT_NAME;
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"Redd_Tank_M120_Tampella","m120_comp_ace","rnt_ace_compatibility","rnt_gmw_static","rnt_mg3_static"};
+        requiredAddons[] = {"tsp_breach_frame","tsp_breach_popper","tsp_breach_stick"};
         // Optional. If this is 1, if any of requiredAddons[] entry is missing in your game the entire config will be ignored and return no error (but in rpt) so useful to make a compat Mod (Since Arma 3 2.14)
         skipWhenMissingDependencies = 1;   
-        author = ECSTRING(main,TacticalTrainingTeam);
-        authors[] = {""};
-        url = ECSTRING(main,URL);
-        VERSION_CONFIG;
     };
 };
 
--- addons/compat_xeh_fix/compat_xeh_fix_pbw/config.cpp
+++ addons/compat_xeh_fix/compat_xeh_fix_pbw/config.cpp
@@ -2,17 +2,14 @@
 
 class CfgPatches {
     class ADDON {
+        addonRootClass = QUOTE(ADDON);
         name = COMPONENT_NAME;
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"A3_Static_F_Sams_Radar_System_01","A3_Static_F_Sams_Radar_System_02","A3_Static_F_Sams_Radar_System_01"};
+        requiredAddons[] = {"PBW_German_Chests"};
         // Optional. If this is 1, if any of requiredAddons[] entry is missing in your game the entire config will be ignored and return no error (but in rpt) so useful to make a compat Mod (Since Arma 3 2.14)
-        skipWhenMissingDependencies = 1;    
-        author = ECSTRING(main,TacticalTrainingTeam);
-        authors[] = {""};
-        url = ECSTRING(main,URL);
-        VERSION_CONFIG;
+        skipWhenMissingDependencies = 1;   
     };
 };
 
