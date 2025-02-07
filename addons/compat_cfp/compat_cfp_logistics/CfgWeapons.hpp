66b2708f0959f5390089db3e4f55366b63b61c22
Andx <andx667@protonmail.com>
2025-02-07 16:40:40 +0100

    reenable

--- addons/compat_cfp/compat_cfp_logistics/CfgWeapons.hpp
+++ addons/compat_cfp/compat_cfp_logistics/CfgWeapons.hpp
@@ -5,7 +5,7 @@ class CfgWeapons {
     class V_PlateCarrier1_rgr;
 
     class CFP_HV2_Ciras_Olive: V_PlateCarrier1_rgr {
-        //ace_logistics_wirecutter_hasWirecutter = 1;
+        ace_logistics_wirecutter_hasWirecutter = 1;
     };
 
 };
