0f8035539168a081f30a15c2934ad754cd4beb36
Andx <andx667@protonmail.com>
2025-02-08 09:36:29 +0100

    add m120

---
+++ addons/compat_rnt/compat_rnt_m120/config.cpp
@@ -0,0 +1,22 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class SUBADDON {
+        authors[] = {"Mishkar", "Andx"};
+        name = COMPONENT_NAME;
+        addonRootClass = QUOTE(ADDON);
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "Redd_Tank_M120_Tampella",
+            "rnt_ace_compatibility"
+        };
+        units[] = {};
+        weapons[] = {};
+        VERSION_CONFIG;
+
+        skipWhenMissingDependencies = 1;
+    };
+};
+
+#include "CfgEventhandlers.hpp"
+#include "CfgVehicles.hpp"
--- addons/compat_rnt/compat_rnt_wiesel_mk20/script_component.hpp
+++ addons/compat_rnt/compat_rnt_wiesel_mk20/script_component.hpp
@@ -1,3 +1,3 @@
 #define SUBCOMPONENT wiesel_mk20
-#define SUBCOMPONENT_BEAUTIFIED Luchs
+#define SUBCOMPONENT_BEAUTIFIED Wiesel MK20
 #include "..\script_component.hpp"
