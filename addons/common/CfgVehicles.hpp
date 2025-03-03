aab53c6e7f7a2791b2e397241ceae25f57bdc2a1
Andx <andx667@protonmail.com>
2025-03-03 11:10:15 +0100

    add stringtable

--- addons/common/CfgVehicles.hpp
+++ addons/common/CfgVehicles.hpp
@@ -1,44 +1,46 @@
-class B_Officer_F;
-class B_Engineer_F;
-class B_Medic_F;
-class B_Crew_F;
-class B_soldier_mine_F;
-class B_Soldier_Repair_F;
+class CfgVehicles {
+    class B_Officer_F;
+    class B_Medic_F;
+    class B_Crew_F;
+    class B_soldier_mine_F;
+    class B_Soldier_Repair_F;
 
-class TTT_B_Commanding_Officer_F : B_Officer_F {
-    displayName = "Commanding Officer";
-};
-class TTT_B_Executive_Officer_F : B_Officer_F {
-    displayName = "Executive Officer";
-};
-class TTT_B_JTAC_F : B_Officer_F {
-    displayName = "JTAC";
-};
-class TTT_B_FO_F : B_Officer_F {
-    displayName = "Forward Observer";
-};
-class TTT_B_RadioOperator_F : B_Officer_F {
-    displayName = "Funker";
+    class TTT_B_Commanding_Officer_F : B_Officer_F {
+        displayName = CSTRING(CO);
+    };
+    class TTT_B_Executive_Officer_F : B_Officer_F {
+        displayName = CSTRING(XO);
+    };
+    class TTT_B_JTAC_F : B_Officer_F {
+        displayName = CSTRING(JTAC);
+    };
+    class TTT_B_FO_F : B_Officer_F {
+        displayName = CSTRING(FO);
+    };
+    class TTT_B_RadioOperator_F : B_Officer_F {
+        displayName = CSTRING(RadioOperator);
+    };
+
+    class TTT_B_Soldier_Repair_TL_F : B_Soldier_Repair_F {
+        displayName = CSTRING(LogTL);
+    };
+
+    class TTT_B_Medic_Doctor_F : B_Medic_F {
+        displayName = CSTRING(Doc);
+    };
+    class TTT_B_Medic_CM_F : B_Medic_F {
+        displayName = CSTRING(CM);
+    };
+
+    class TTT_B_Crew_TL_F : B_Crew_F {
+        displayName = CSTRING(CrewTL);
+    };
+
+    class TTT_B_EOD_F: B_soldier_mine_F{
+        displayName = CSTRING(EOD);
+    };
+    class TTT_B_EOD_TL_F: TTT_B_EOD_F{
+        displayName = CSTRING(EODTL);
+    };
 };
 
-class TTT_B_Soldier_Repair_TL_F : B_Soldier_Repair_F {
-    displayName = "Logistik Teamlead";
-};
-
-class TTT_B_Medic_Doctor_F : B_Medic_F {
-    displayName = "Doktor";
-};
-class TTT_B_Medic_CM_F : B_Medic_F {
-    displayName = "Combat Medic";
-};
-
-class TTT_B_Crew_TL_F : B_Crew_F {
-    displayName = "Panzerkommandant";
-};
-
-class TTT_B_EOD_F: B_soldier_mine_F{
-    displayName = "EOD";
-};
-class TTT_B_EOD_TL_F: TTT_B_EOD_F{
-    displayName = "EOD Teamleader";
-};
--- addons/common/config.cpp
+++ addons/common/config.cpp
@@ -8,13 +8,13 @@ class CfgPatches {
             "TTT_B_Executive_Officer_F",
             "TTT_B_JTAC_F",
             "TTT_B_FO_F",
-            "TTT_B_Engineer_TL_F",
+            "TTT_B_RadioOperator_F",
+            "TTT_B_Soldier_Repair_TL_F",
             "TTT_B_Crew_TL_F",
             "TTT_B_Medic_CM_F",
             "TTT_B_Medic_Doctor_F",
             "TTT_B_EOD_F",
             "TTT_B_EOD_TL_F"
-
         };
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
