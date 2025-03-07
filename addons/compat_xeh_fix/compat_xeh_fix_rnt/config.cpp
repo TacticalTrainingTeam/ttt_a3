e95b00ac1a0f22650416d6c387feeb4d3d6d8c46
Andx <andx667@protonmail.com>
2025-03-07 10:38:47 +0100

    add line break

--- addons/compat_xeh_fix/compat_xeh_fix_rnt/config.cpp
+++ addons/compat_xeh_fix/compat_xeh_fix_rnt/config.cpp
@@ -12,7 +12,8 @@ class CfgPatches {
             "Redd_Tank_M120_Tampella",
             "m120_comp_ace",
             "rnt_ace_compatibility",
-            "rnt_gmw_static","rnt_mg3_static"
+            "rnt_gmw_static",
+            "rnt_mg3_static"
         };
         // Optional. If this is 1, if any of requiredAddons[] entry is missing in your game the entire config will be ignored and return no error (but in rpt) so useful to make a compat Mod (Since Arma 3 2.14)
         skipWhenMissingDependencies = 1;   
