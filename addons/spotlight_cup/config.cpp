7ee643363d3606c92370cd8b867078c13b96516d
Andx667 <andx667@protonmail.com>
2025-03-09 18:44:53 +0100

    add required addons and skipwhenmissing

--- addons/spotlight_cup/config.cpp
+++ addons/spotlight_cup/config.cpp
@@ -12,11 +12,14 @@ class CfgPatches {
         requiredVersion = REQUIRED_VERSION;
         // Required addons, used for setting load order. (CfgPatches classname NOT PBO filename!)
         // When any of the addons are missing, a pop-up warning will appear when launching the game.
-        requiredAddons[] = {"cba_ui", "ttt_common"};
+        requiredAddons[] = {"cba_ui", "ttt_common","CUP_Creatures_People_LoadOrder"};
         // List of objects (CfgVehicles classes) contained in the addon. Important also for Zeus content (units and groups) unlocking.
         units[] = {};
         // List of weapons (CfgWeapons classes) contained in the addon.
         weapons[] = {};
+    
+        // Optional. If this is 1, if any of requiredAddons[] entry is missing in your game the entire config will be ignored and return no error (but in rpt) so useful to make a compat Mod (Since Arma 3 2.14)
+        skipWhenMissingDependencies = 1;
         VERSION_CONFIG;
     };
 };
