db6319676a06e9a0b87b2d3f4ddab276b07c3de6
Andx667 <andx667@protonmail.com>
2025-03-09 21:41:38 +0100

    enable and update readme, add skip

--- addons/compat_cup_units/compat_cup_units_trenches/config.cpp
+++ addons/compat_cup_units/compat_cup_units_trenches/config.cpp
@@ -2,14 +2,13 @@
 
 class CfgPatches {
     class SUBADDON {
-        author = ECSTRING(main,TacticalTrainingTeam);
-        authors[] = {"Andx"};
-        name = COMPONENT_NAME;
         addonRootClass = QUOTE(ADDON);
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"ace_trenches", "CUP_Creatures_People_LoadOrder"};
         units[] = {};
         weapons[] = {};
+        // Optional. If this is 1, if any of requiredAddons[] entry is missing in your game the entire config will be ignored and return no error (but in rpt) so useful to make a compat Mod (Since Arma 3 2.14)
+        skipWhenMissingDependencies = 1;
         VERSION_CONFIG;
     };
 };
--- addons/compat_cup_units/readme.md
+++ addons/compat_cup_units/readme.md
@@ -1,5 +1,9 @@
 # Compat CUP Units
 
+## Trenches
+
+Fügt einigen Westen und Rucksäcken die Klappspatenfunktion hinzu.
+
 ## Maintainer
 
 - Andx
