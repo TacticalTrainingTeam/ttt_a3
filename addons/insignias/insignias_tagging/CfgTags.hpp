525ba5cf85ac4d20fd3f4c43d6ff09f795880599
Andx <andx667@protonmail.com>
2025-03-14 19:37:39 +0100

    fix grey error

--- addons/insignias/insignias_tagging/CfgTags.hpp
+++ addons/insignias/insignias_tagging/CfgTags.hpp
@@ -99,11 +99,11 @@ class ACE_Tags {
     };
 
     class insignia_grey {
-        displayName = ECSTRING(insignias,grey_emblem_displayName);  // Name of your tag being displayed in the interaction menu
+        displayName = ECSTRING(insignias,grau_emblem_displayName);  // Name of your tag being displayed in the interaction menu
         requiredItem = "ACE_SpraypaintBlack";  // Required item to have in the inventory to be able to spray your tag (eg. `"ACE_SpraypaintBlack"`, `"ACE_SpraypaintRed"`, `"ACE_SpraypaintGreen"`, `"ACE_SpraypaintBlue"` or any custom item from `CfgWeapons`)
-        textures[] = {QPATHTOEF(insignias,data\grey.paa)};  // List of texture variations (one is randomly selected when tagging)
+        textures[] = {QPATHTOEF(insignias,data\grau.paa)};  // List of texture variations (one is randomly selected when tagging)
         materials[] = {}; // Optional: List of material variations (one is randomly selected). Keep empty if you don't need a custom material.
-        icon = QPATHTOEF(insignias,data\grey.paa);  // Icon being displayed in the interaction menu
+        icon = QPATHTOEF(insignias,data\grau.paa);  // Icon being displayed in the interaction menu
         //tagModel = "UserTexture1m_F"; // Optional: The 3D Model that will be spawned with the texture on it, can either be CfgVehicles classname or P3D file path.
     };
 };
