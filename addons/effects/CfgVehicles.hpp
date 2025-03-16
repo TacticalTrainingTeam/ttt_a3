dd935487d090a79e9672524ec4a2d8e2ca1a6c2a
Andx <andx667@protonmail.com>
2025-03-16 11:19:23 +0100

    tabs to spaces

--- addons/effects/CfgVehicles.hpp
+++ addons/effects/CfgVehicles.hpp
@@ -6,24 +6,24 @@ class CfgVehicles {
         category = "TTT";
         function = QFUNC(stormInit);
         functionPriority = 10;
-		isGlobal = 0;						// 0 for server only execution, 1 for global execution, 2 for persistent global execution
-		isTriggerActivated = 1;				// 1 for module waiting until all synced triggers are activated
-		isDisposable = 1;					// 1 if module is to be disabled once it is activated (i.e. repeated trigger activation will not work)
+        isGlobal = 0;                        // 0 for server only execution, 1 for global execution, 2 for persistent global execution
+        isTriggerActivated = 1;                // 1 for module waiting until all synced triggers are activated
+        isDisposable = 1;                    // 1 if module is to be disabled once it is activated (i.e. repeated trigger activation will not work)
         author = "Andx";
     };
 
     class Module_F;
     class GVAR(initStormModule_Vanilla): Module_F {
-		scope = 2;										// Editor visibility; 2 will show it in the menu, 1 will hide it.
-		displayName = "Sandstorm Modul Vanilla";			// Name displayed in the menu
-		//icon = "\TAG_addonName\data\icon_Nuke_ca.paa";	// Map icon. Delete this entry to use the default icon.
-		category = "TTT";
+        scope = 2;                                        // Editor visibility; 2 will show it in the menu, 1 will hide it.
+        displayName = "Sandstorm Modul Vanilla";            // Name displayed in the menu
+        //icon = "\TAG_addonName\data\icon_Nuke_ca.paa";    // Map icon. Delete this entry to use the default icon.
+        category = "TTT";
 
-		function = QFUNC(stormInit);	// Name of function triggered once conditions are met
-		functionPriority = 10;				// Execution priority, modules with lower number are executed first. 0 is used when the attribute is undefined
-		isGlobal = 0;						// 0 for server only execution, 1 for global execution, 2 for persistent global execution
-		isTriggerActivated = 1;				// 1 for module waiting until all synced triggers are activated
-		isDisposable = 1;					// 1 if modules is to be disabled once it is activated (i.e. repeated trigger activation will not work)
+        function = QFUNC(stormInit);    // Name of function triggered once conditions are met
+        functionPriority = 10;                // Execution priority, modules with lower number are executed first. 0 is used when the attribute is undefined
+        isGlobal = 0;                        // 0 for server only execution, 1 for global execution, 2 for persistent global execution
+        isTriggerActivated = 1;                // 1 for module waiting until all synced triggers are activated
+        isDisposable = 1;                    // 1 if modules is to be disabled once it is activated (i.e. repeated trigger activation will not work)
     };
 
 };
