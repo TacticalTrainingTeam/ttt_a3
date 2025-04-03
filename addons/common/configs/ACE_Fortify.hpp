312dd68fc739a3ef50aac412971c3415bb921be4
Andx667 <andx667@protonmail.com>
2025-04-03 17:15:27 +0200

    initial commit

---
+++ addons/common/configs/ACE_Fortify.hpp
@@ -0,0 +1,19 @@
+class ACEX_Fortify_Presets {
+    class TTT_preset_ohk {
+        displayName = "TTT - OHK";
+        objects[] = {
+            {"EFM_wood_wall_2m_single_filled", 5},
+            {"Land_BagFence_Short_F", 2},
+            {"Land_Net_Fence_4m_F", 5},
+            {"Land_Plank_01_4m_F", 1}
+        };
+    };
+
+    class TTT_preset_FOB {
+        displayName = "TTT - FOB";
+        objects[] = {
+            {"Land_BagFence_Short_F", 2},
+            {"Land_BagBunker_Tower_F", 10}
+        };
+    };
+};
