b58514a84b2091c34523df5b4c290cd96b7b61fe
Andx667 <andx667@protonmail.com>
2025-04-03 17:26:24 +0200

    add fob objects

--- addons/common/configs/ACE_Fortify.hpp
+++ addons/common/configs/ACE_Fortify.hpp
@@ -1,5 +1,5 @@
 class ACEX_Fortify_Presets {
-    class TTT_preset_ohk {
+    class TTT_ohk {
         displayName = "TTT - OHK";
         objects[] = {
             {"EFM_wood_wall_2m_single_filled", 5},
@@ -9,11 +9,51 @@ class ACEX_Fortify_Presets {
         };
     };
 
-    class TTT_preset_FOB {
+    class TTT_fob {
         displayName = "TTT - FOB";
         objects[] = {
-            {"Land_BagFence_Short_F", 2},
-            {"Land_BagBunker_Tower_F", 10}
+            {"Land_MedicalTent_01_NATO_generic_outer_F", 5},
+            {"Land_Cargo_Patrol_V1_F", 10},
+            {"Land_Cargo_House_V1_F", 15},
+            {"Land_Medevac_house_V1_F", 20},
+            {"Land_Cargo_Tower_V1_F", 30},
+            {"CamoNet_BLUFOR_F", 2},
+            {"CamoNet_BLUFOR_open_F", 2},
+            {"CamoNet_BLUFOR_big_F", 3},
+            {"Land_PortableLight_single_F", 1},
+            {"Land_PortableLight_double_F", 2},
+            {"Land_LampStreet_small_F", 2},
+            {"Land_LampAirport_F", 3},
+            {"Land_HelipadCircle_F", 10},
+            {"Land_HelipadRescue_F", 12},
+            {"PortableHelipadLight_01_blue_F", 5},
+            {"PortableHelipadLight_01_green_F", 5},
+            {"PortableHelipadLight_01_red_F", 5},
+            {"Land_CampingChair_V1_F", 5},
+            {"Land_CampingChair_V2_F", 5},
+            {"Land_CampingTable_F", 10},
+            {"Land_BagFence_Round_F", 2},
+            {"Land_BagFence_Short_F", 1},
+            {"Land_BagFence_Long_F", 2},
+            {"Land_BagFence_Corner_F", 2},
+            {"Land_BagFence_End_F", 1},
+            {"Land_BagBunker_Small_F", 5},
+            {"Land_BagBunker_Large_F", 10},
+            {"Land_BagBunker_Tower_F", 15},
+            {"Land_HBarrier_1_F", 1},
+            {"Land_HBarrier_3_F", 3},
+            {"Land_HBarrier_5_F", 4},
+            {"Land_HBarrier_Big_F", 6},
+            {"Land_HBarrierWall4_F", 5},
+            {"Land_HBarrierWall6_F", 8},
+            {"Land_HBarrierWall_corner_F", 8},
+            {"Land_HBarrierWall_corridor_F", 9},
+            {"Land_HBarrierTower_F", 20},
+            {"Land_CncShelter_F", 10},
+            {"Land_CncWall1_F", 2},
+            {"Land_CncWall4_F", 5},
+            {"Land_Razorwire_F", 2},
+            {"Land_ClutterCutter_large_F", 1}
         };
     };
 };
