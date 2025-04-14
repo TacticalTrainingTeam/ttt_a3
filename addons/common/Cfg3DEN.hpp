d29e3638e4566058be8a7912ad15de7b296a9190
MenomDE <36950652+MenomDE@users.noreply.github.com>
2025-04-14 12:19:48 +0200

    Cfg3DEN.hpp aktualisieren

--- addons/common/Cfg3DEN.hpp
+++ addons/common/Cfg3DEN.hpp
@@ -5,7 +5,7 @@ class Cfg3DEN {
             side = 8;                                            // 0 opfor, 1 blufor, 2 indfor, 3 civ, 8 Empty/Props
             editorCategory = "EdCat_Default";      // link to CfgEditorCategories
             editorSubcategory = QEGVAR(main,TTT);     // link to CfgEditorSubcategories
-            displayName = "TTT Basismodule";
+            displayName = "TTT-Basismodule";
             icon = "\A3\ui_f\data\map\markers\nato\b_hq.paa";    // left side icon in groups list
             useSideColorOnIcon = 1;                              // 1 == icon is always colored in faction color
         };
