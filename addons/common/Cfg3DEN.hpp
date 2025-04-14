d9bb5496d4d134c2c3717b5f24797382b04e1a5f
Andy Börner <17100110+Andx667@users.noreply.github.com>
2025-04-14 12:21:03 +0200

    common - Fix-composition (#314)
    * change names
    
    * fixed display in editor
    
    * update readme
    
    * minor beautification
    
    * Cfg3DEN.hpp aktualisieren
    
    ---------
    
    Co-authored-by: MenomDE <36950652+MenomDE@users.noreply.github.com>

--- addons/common/Cfg3DEN.hpp
+++ addons/common/Cfg3DEN.hpp
@@ -1,11 +1,11 @@
 class Cfg3DEN {
     class Compositions {
         class TTT_basisModule {                                  // one class per composition
-            path = "compositions\TTT-Basis-Module";              // pbo path to a folder containing header.sqe/composition.sqe files
+            path = "z\ttt\addons\common\compositions\basisModule";              // pbo path to a folder containing header.sqe/composition.sqe files
             side = 8;                                            // 0 opfor, 1 blufor, 2 indfor, 3 civ, 8 Empty/Props
-            //editorCategory = QGVAR(TacticalTrainingTeam);      // link to CfgEditorCategories
-            //editorSubcategory = QGVAR(TacticalTrainingTeam);     // link to CfgEditorSubcategories
-            //displayName = "$STR_Composition_Armored01";
+            editorCategory = "EdCat_Default";      // link to CfgEditorCategories
+            editorSubcategory = QEGVAR(main,TTT);     // link to CfgEditorSubcategories
+            displayName = "TTT-Basismodule";
             icon = "\A3\ui_f\data\map\markers\nato\b_hq.paa";    // left side icon in groups list
             useSideColorOnIcon = 1;                              // 1 == icon is always colored in faction color
         };
