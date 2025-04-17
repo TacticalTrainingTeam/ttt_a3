7b00cfa9c0f934a3f92ddccfa7fd1eb4b97437da
Andy Börner <andy.boener@informatik.tu-chemnitz.de>
2025-04-17 14:04:30 +0200

    fix config style

--- addons/compat_cup_vehicles/compat_cup_vehicles_itc/config.cpp
+++ addons/compat_cup_vehicles/compat_cup_vehicles_itc/config.cpp
@@ -3,29 +3,29 @@
 class CfgPatches {
     class SUBADDON {
         addonRootClass = QUOTE(ADDON);
-		units[] = {	
-			"itc_land_cup_m270_ba_wood_he",
-			"itc_land_cup_m270_ba_wood_pgm",
-			"itc_land_cup_m270_ba_des_he",
-			"itc_land_cup_m270_ba_des_pgm",
-			"itc_land_cup_m270_hil_he",
-			"itc_land_cup_m270_hil_pgm",
-			"itc_land_cup_m270_usa_he",
-			"itc_land_cup_m270_usa_pgm",
-			"itc_land_cup_m270_usmc_he",
-			"itc_land_cup_m270_usmc_pgm",
-			"itc_land_cup_m270_aaf_he",
-			"itc_land_cup_m270_aaf_pgm",
-			"itc_land_cup_m270_racs_he",
-			"itc_land_cup_m270_racs_pgm"
- 		};
-		weapons[] = {};
-		requiredVersion = REQUIRED_VERSION;
-		requiredAddons[] = {"A3_Armor_F_Slammer", "CUP_TrackedVehicles_M270", "itc_land_common"};
-		authors[] = {"Addi"};
-		VERSION_CONFIG;
+        units[] = {    
+            "itc_land_cup_m270_ba_wood_he",
+            "itc_land_cup_m270_ba_wood_pgm",
+            "itc_land_cup_m270_ba_des_he",
+            "itc_land_cup_m270_ba_des_pgm",
+            "itc_land_cup_m270_hil_he",
+            "itc_land_cup_m270_hil_pgm",
+            "itc_land_cup_m270_usa_he",
+            "itc_land_cup_m270_usa_pgm",
+            "itc_land_cup_m270_usmc_he",
+            "itc_land_cup_m270_usmc_pgm",
+            "itc_land_cup_m270_aaf_he",
+            "itc_land_cup_m270_aaf_pgm",
+            "itc_land_cup_m270_racs_he",
+            "itc_land_cup_m270_racs_pgm"
+         };
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"A3_Armor_F_Slammer", "CUP_TrackedVehicles_M270", "itc_land_common"};
+        authors[] = {"Addi"};
+        VERSION_CONFIG;
         skipWhenMissingDependencies = 1; // Skip this addon if the required addons are not present
-	};
+    };
 };
 
 #include "CfgITCLandBalistics.hpp"
