50df8c9fe9c4b01faf04a11c8ccb65c5b2360e87
Andx <andx667@protonmail.com>
2025-05-12 16:40:44 +0200

    remove

--- addons/compat_cfp/script_component.hpp
+++
@@ -1,5 +0,0 @@
-#define COMPONENT compat_cfp_vehicles
-#define COMPONENT_BEAUTIFIED Compat CFP Vehicles
-#include "\z\ttt\addons\main\script_mod.hpp"
-
-#include "\z\ttt\addons\main\script_macros.hpp"
