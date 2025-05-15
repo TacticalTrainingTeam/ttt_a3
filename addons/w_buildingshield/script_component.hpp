1cc17e08411afcf0f82ee55068e8a3b5aa7bd5a8
Andy <andx667@protonmail.com>
2025-05-15 11:29:16 +0200

    rename folder  and empty line at eof

--- addons/w_buildingshield/script_component.hpp
+++
@@ -1,5 +0,0 @@
-#define COMPONENT w_buildingshield
-#define COMPONENT_BEAUTIFIED W-Building Shield
-
-#include "\z\ttt\addons\main\script_mod.hpp"
-#include "\z\ttt\addons\main\script_macros.hpp"
---
+++ addons/w_shields/readme.md
@@ -0,0 +1,9 @@
+# W-Shields
+
+## Referenzen
+
+<https://gitlab.gruppe-w.de/Missionsbau/Framework/-/tree/master/addons/shield?ref_type=heads>
+
+## Maintainer
+
+- Andx
