b5be47eeea29ff05eaa04c74caa50f27833b584c
Andy <andx667@protonmail.com>
2025-05-21 11:23:14 +0200

    add pictures and config

---
+++ addons/common/configs/ACE_Tagging.hpp
@@ -0,0 +1,62 @@
+class ACE_Tags {
+    class GVAR(numberZero) {
+        displayName = "0";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOF(data\Zahl-00_ca.paa)};
+        icon = QPATHTOF(data\Zahl-00_ca.paa);
+    };
+    class GVAR(numberOne) {
+        displayName = "1";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOF(data\Zahl-01_ca.paa)};
+        icon = QPATHTOF(data\Zahl-01_ca.paa);
+    };
+    class GVAR(numberTwo) {
+        displayName = "2";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOF(data\Zahl-02_ca.paa)};
+        icon = QPATHTOF(data\Zahl-02_ca.paa);
+    };
+    class GVAR(numberThree) {
+        displayName = "3";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOF(data\Zahl-03_ca.paa)};
+        icon = QPATHTOF(data\Zahl-03_ca.paa);
+    };
+    class GVAR(numberFour) {
+        displayName = "4";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOF(data\Zahl-04_ca.paa)};
+        icon = QPATHTOF(data\Zahl-04_ca.paa);
+    };
+    class GVAR(numberFive) {
+        displayName = "5";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOF(data\Zahl-05_ca.paa)};
+        icon = QPATHTOF(data\Zahl-05_ca.paa);
+    };
+    class GVAR(numberSix) {
+        displayName = "6";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOF(data\Zahl-06_ca.paa)};
+        icon = QPATHTOF(data\Zahl-06_ca.paa);
+    };
+    class GVAR(numberSeven) {
+        displayName = "7";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOF(data\Zahl-07_ca.paa)};
+        icon = QPATHTOF(data\Zahl-07_ca.paa);
+    };
+    class GVAR(numberEight) {
+        displayName = "8";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOF(data\Zahl-08_ca.paa)};
+        icon = QPATHTOF(data\Zahl-08_ca.paa);
+    };
+    class GVAR(numberNine) {
+        displayName = "9";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOF(data\Zahl-09_ca.paa)};
+        icon = QPATHTOF(data\Zahl-09_ca.paa);
+    };
+};
