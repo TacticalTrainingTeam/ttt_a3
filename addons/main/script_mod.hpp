b4bbf8b676a79d05b3927d0aff9eea7e9b015315
Andx <andx667@protonmail.com>
2025-06-22 10:57:05 +0200

    update cba version

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -12,7 +12,7 @@
 
 // MINIMAL required version for the Mod. Components can specify others..
 #define REQUIRED_VERSION 2.20
-#define REQUIRED_CBA_VERSION {3,18,0}
+#define REQUIRED_CBA_VERSION {3,18,3}
 
 #ifndef COMPONENT_BEAUTIFIED
     #define COMPONENT_BEAUTIFIED COMPONENT
