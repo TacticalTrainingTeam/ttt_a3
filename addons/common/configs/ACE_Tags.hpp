7a9e3dc7fb2d19043ba5a3bd3e7bd63d0e684e7d
Andy <andx667@protonmail.com>
2025-06-25 13:41:10 +0200

    apply suggestions from code review

--- addons/common/configs/ACE_Tags.hpp
+++ addons/common/configs/ACE_Tags.hpp
@@ -11,7 +11,7 @@ class ACE_Tags {
         requiredItem = QGVAR(spraypaintMemes);  // Required item to have in the inventory to be able to spray your tag (eg. `"ACE_SpraypaintBlack"`, `"ACE_SpraypaintRed"`, `"ACE_SpraypaintGreen"`, `"ACE_SpraypaintBlue"` or any custom item from `CfgWeapons`)
         textures[] = {QPATHTOF(data\p_nettHier_ca.paa)};  // List of texture variations (one is randomly selected when tagging)
         materials[] = {}; // Optional: List of material variations (one is randomly selected). Keep empty if you don't need a custom material.
-        icon = QPATHTOF(cdata\p_nettHier_ca.paa);  // Icon being displayed in the interaction menu
+        icon = QPATHTOF(data\p_nettHier_ca.paa);  // Icon being displayed in the interaction menu
         //tagModel = "UserTexture1m_F"; // Optional: The 3D Model that will be spawned with the texture on it, can either be CfgVehicles classname or P3D file path.
     };
 
--- addons/insignias/config.cpp
+++ addons/insignias/config.cpp
@@ -14,11 +14,11 @@ class CfgPatches {
         // When any of the addons are missing, a pop-up warning will appear when launching the game.
         requiredAddons[] = {"ttt_common","ace_tagging"};
         // List of objects (CfgVehicles classes) contained in the addon. Important also for Zeus content (units and groups) unlocking.
-        units[] = {
+        units[] = { };
+        // List of weapons (CfgWeapons classes) contained in the addon.
+        weapons[] = {
             QGVAR(spraypaint),
         };
-        // List of weapons (CfgWeapons classes) contained in the addon.
-        weapons[] = { };
 
         // Optional. If this is 1, if any of requiredAddons[] entry is missing in your game the entire config will be ignored and return no error (but in rpt) so useful to make a compat Mod (Since Arma 3 2.14)
         //skipWhenMissingDependencies = 1;
