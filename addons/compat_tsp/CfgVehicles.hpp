28f971ba377d8bc2bd21cabd0dba0214b4c69642
Andx <andx667@protonmail.com>
2025-07-20 10:29:29 +0200

    tab to spaces

--- addons/compat_tsp/CfgVehicles.hpp
+++ addons/compat_tsp/CfgVehicles.hpp
@@ -1,15 +1,15 @@
 class CfgVehicles {  
     class Man;
-	class CAManBase: Man {
-		class ACE_SelfActions {
-			class ACE_Equipment	{
-				class tsp_ace_earplug_insert {
-					condition = false;
-				};
-				class tsp_ace_earplug_remove: tsp_ace_earplug_insert {
+    class CAManBase: Man {
+        class ACE_SelfActions {
+            class ACE_Equipment    {
+                class tsp_ace_earplug_insert {
+                    condition = false;
+                };
+                class tsp_ace_earplug_remove: tsp_ace_earplug_insert {
                     condition = false
                 };
-			};
-		};
-	};
+            };
+        };
+    };
 };
