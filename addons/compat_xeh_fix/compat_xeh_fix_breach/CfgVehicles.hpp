e11b31cb600170f14373edc00230beb4bb01a222
Andx <andx667@protonmail.com>
2025-07-20 10:23:49 +0200

    initial commit

--- addons/compat_xeh_fix/compat_xeh_fix_breach/CfgVehicles.hpp
+++ addons/compat_xeh_fix/compat_xeh_fix_breach/CfgVehicles.hpp
@@ -12,5 +12,8 @@ class CfgVehicles {
     class tsp_breach_popper_auto_place: tsp_breach_popper_place { XEH_ENABLED; }; // "M4A1 SLAM" @Breach_Rewrite
     class tsp_breach_silhouette_place: ACE_Explosives_Place_SLAM { XEH_ENABLED; }; // "M4A1 SLAM" @Breach_Rewrite
     class tsp_breach_stick_place: ACE_Explosives_Place_SLAM { XEH_ENABLED; }; // "M4A1 SLAM" @Breach_Rewrite
+
+    class tsp_breach_dip_place: ACE_Explosives_Place_SLAM { XEH_ENABLED; }; // "M4A1 SLAM" @Breach_Rewrite
+    class tsp_breach_dip_auto_place: tsp_breach_dip_place { XEH_ENABLED; }; // "M4A1 SLAM" @Breach_Rewrite
 };
 
