4623518ffa5aeca697acad8aead144e5c3a55f93
GWagner2021 <81987613+GWagner2021@users.noreply.github.com>
2025-08-06 00:54:07 +0200

    Update CfgVehicles.hpp

--- addons/backpacks/CfgVehicles.hpp
+++ addons/backpacks/CfgVehicles.hpp
@@ -75,5 +75,25 @@ class CfgVehicles {
         picture = QPATHTOF(data\pictures\ttt_backpack_radio_yellow_us_desert_ca.paa);
         hiddenSelectionsTextures[] = {QPATHTOF(data\ttt_backpack_radio_yellow_us_desert_co.paa)};
     };
+
+    class medic_backpack_Base: B_Kitbag_tan {
+        author = ECSTRING(main,TacticalTrainingTeam);
+        scope = 0;
+        maximumLoad = 380;
+        mass = 30;
+    };
+
+    class medic_01_backpack_fast_tan_co: medic_backpack_Base {
+        scope = 2;
+        displayName = CSTRING(medic_01_backpack_fast_tan_co);
+        picture = QPATHTOF(data\pictures\medic_01_backpack_fast_tan_co_ca.paa);
+        hiddenSelectionsTextures[] = {QPATHTOF(data\medic_01_backpack_fast_tan_co.paa)};
+    };
+
+    class medic_02_backpack_fast_tan_co: medic_01_backpack_fast_tan_co {
+        displayName = CSTRING(medic_02_backpack_fast_tan_co);
+        picture = QPATHTOF(data\pictures\medic_02_backpack_fast_tan_co_ca.paa);
+        hiddenSelectionsTextures[] = {QPATHTOF(data\medic_02_backpack_fast_tan_co.paa)};
+    };
 };
 
