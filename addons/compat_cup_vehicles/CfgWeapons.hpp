7eefb1b0c7a8597aa22d574d0e138bfbc37969d0
Andy <andx667@protonmail.com>
2025-09-17 13:57:15 +0200

    initial commit

--- addons/compat_cup_vehicles/CfgWeapons.hpp
+++ addons/compat_cup_vehicles/CfgWeapons.hpp
@@ -1,19 +1,16 @@
 class CfgWeapons {
     //Made by martin509
-    class RocketPods;
-    class rockets_230mm_GAT : RocketPods {
-        class Close;
-    };
-    class CUP_Vmlauncher_GRAD_veh  : rockets_230mm_GAT{
+    class rockets_230mm_GAT;
+    class CUP_Vmlauncher_GRAD_veh: rockets_230mm_GAT {
         class Close;
     };
     class CUP_Vmlauncher_technical: CUP_Vmlauncher_GRAD_veh {
-        class Close : Close {
+        class Close: Close {
             artilleryDispersion = 8;
         };
     };
     class CUP_Vmlauncher_ub32_technical: CUP_Vmlauncher_GRAD_veh {
-        class Close : Close {
+        class Close: Close {
             artilleryDispersion = 12;
             aiRateOfFire=0.35;
             aiRateOfFireDistance=1054;
