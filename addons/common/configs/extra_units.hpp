bed30b0f2ad0bc74edec1b317531f0133f7e5f57
Andx <andx667@protonmail.com>
2025-09-22 17:50:59 +0200

    remove fully

--- addons/common/configs/extra_units.hpp
+++
@@ -1,7 +0,0 @@
-//Custom Units
-//Used for Unit-based Loadoutsystems like poppy or grad-loadout
-class B_Officer_F;
-
-class TTT_B_RadioOperator_F: B_Officer_F {
-    displayName = CSTRING(RadioOperator);
-};
