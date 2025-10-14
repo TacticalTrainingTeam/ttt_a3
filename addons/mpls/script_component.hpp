f70dd3459b9fb8f3d69281eb2da3acd6490f8a11
Andx667 <andx667@protonmail.com>
2025-10-14 20:56:57 +0200

    change numed to macros for better identification

--- addons/mpls/script_component.hpp
+++ addons/mpls/script_component.hpp
@@ -3,3 +3,8 @@
 
 #include "\z\ttt\addons\main\script_mod.hpp"
 #include "\z\ttt\addons\main\script_macros.hpp"
+
+#define TIME_SAVE_FIRST 30
+#define TIME_SAVE_START 30
+#define TIME_SAVE_INTERVAL 600
+#define TIME_AFTER_RESPAWN 10
