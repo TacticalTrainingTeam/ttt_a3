5e5d07df61c9b1a63b1019a46e49ef7e14fbb9a5
Andx <andx667@protonmail.com>
2025-11-27 22:12:26 +0100

    init

---
+++ addons/no_chain/CfgRsc.hpp
@@ -0,0 +1,26 @@
+class RscPendingInvitation {
+    textureConnectionQualityPoor = QPATHTOF(empty.paa);
+    textureConnectionQualityBad = QPATHTOF(empty.paa);
+    textureDesyncLow = QPATHTOF(empty.paa);
+    textureDesyncHigh = QPATHTOF(empty.paa);
+    color[] = {1, 1, 1, 1};
+    x = 0.939;
+    y = 0.852;
+    w = 0.054;
+    h = 0.072;
+    timeout = 10;
+    blinkingPeriod = 2;
+};
+class RscPendingInvitationInGame {
+    textureConnectionQualityPoor = QPATHTOF(empty.paa);
+    textureConnectionQualityBad = QPATHTOF(empty.paa);
+    textureDesyncLow = QPATHTOF(empty.paa);
+    textureDesyncHigh = QPATHTOF(empty.paa);
+    color[] = {1, 1, 1, 1};
+    x = 0.939;
+    y = 0.852;
+    w = 0.054;
+    h = 0.072;
+    timeout = 10;
+    blinkingPeriod = 2;
+};
