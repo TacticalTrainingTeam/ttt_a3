49b85cdf85ee55707eb169695ed996d3ad67681a
Andx <andx667@protonmail.com>
2025-12-07 17:00:58 +0100

    use macro in afrf

--- addons/compat_rhs/afrf_equipment_balance_compat/script_component.hpp
+++ addons/compat_rhs/afrf_equipment_balance_compat/script_component.hpp
@@ -2,3 +2,8 @@
 #define SUBCOMPONENT_BEAUTIFIED AFRF
 
 #include "..\script_component.hpp"
+
+// Backpack balance macro
+#define BACKPACK_BALANCE(maxLoad,mass)\
+    maximumLoad = maxLoad; \
+    mass = mass
