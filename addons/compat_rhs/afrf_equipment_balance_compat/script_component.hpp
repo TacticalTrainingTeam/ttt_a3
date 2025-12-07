02dfb7b135c918b903cb5d35d9d1d80bbb5cf70c
Andx <andx667@protonmail.com>
2025-12-07 17:29:59 +0100

    use macro for backpacks

--- addons/compat_rhs/afrf_equipment_balance_compat/script_component.hpp
+++ addons/compat_rhs/afrf_equipment_balance_compat/script_component.hpp
@@ -2,8 +2,3 @@
 #define SUBCOMPONENT_BEAUTIFIED AFRF
 
 #include "..\script_component.hpp"
-
-// Backpack balance macro
-#define BACKPACK_BALANCE(maxLoad,mass)\
-    maximumLoad = maxLoad; \
-    mass = mass
--- addons/compat_rhs/gref_equipment_balance_compat/CfgVehicles.hpp
+++ addons/compat_rhs/gref_equipment_balance_compat/CfgVehicles.hpp
@@ -10,71 +10,54 @@ class CfgVehicles {
     class B_Carryall_oli;
     //PACKS
     class rhsgref_cdf_backpack_mg: B_Carryall_oli {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhsgref_cdf_backpack_mg2: B_Carryall_oli {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhsgref_cdf_backpack_mg3: B_Carryall_oli {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhsgref_cdf_backpack_engineer: B_Carryall_oli {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhsgref_cdf_backpack_medic: B_Carryall_oli {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhsgref_nat_backpack_mg: B_AssaultPack_blk {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhsgref_nat_backpack_mg42: rhsusf_falconii {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhsgref_nat_backpack_engineer: B_Carryall_oli {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhsgref_nat_backpack_medic: B_TacticalPack_oli {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhsgref_nat_backpack_explosives: B_TacticalPack_oli {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhsgref_ins_pack: B_FieldPack_khk {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhsgref_ins_pack_GR: B_FieldPack_cbr {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhsgref_ins_packMG: B_TacticalPack_blk {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhsgref_tortila_specter: B_Carryall_Base {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhsgref_hidf_alicepack: Bag_Base {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhsgref_wdl_alicepack: rhsgref_hidf_alicepack {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhsgref_ttsko_alicepack: rhsgref_hidf_alicepack {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
 };
