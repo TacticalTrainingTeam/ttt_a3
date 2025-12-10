44f3565c7625e7dd23ce29c5a241a9bd9939a2ce
Andx <andx667@protonmail.com>
2025-12-10 14:34:02 +0100

    made bolt cutter cut wire

--- addons/compat_usp/CfgWeapons.hpp
+++ addons/compat_usp/CfgWeapons.hpp
@@ -8,4 +8,10 @@ class CfgWeapons {
         //Wenn per ACE Action entfernt wird, ist es weg statt im Inventar -> verstecken
         scope = 0;
     };
+
+    class USP_ItemCore;
+    class USP_BOLTCUTTER: USP_ItemCore {
+        ACE_isTool = 1;
+        ACE_isWirecutter = 1;
+    };
 };
