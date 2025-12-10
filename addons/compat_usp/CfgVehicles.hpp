f2690eb5b19ae325024ad75ccf1af6cbccae7c8e
Andx <andx667@protonmail.com>
2025-12-10 14:29:34 +0100

    balance backpacks

--- addons/compat_usp/CfgVehicles.hpp
+++ addons/compat_usp/CfgVehicles.hpp
@@ -1,41 +1,43 @@
 class CfgVehicles {
     class Bag_Base;
+
+    //Gleiche Werte wie Kitabag aus Vanilla
     class USP_REEBOW_3DAP_BLK: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,50);
     };
     class USP_REEBOW_3DAP_ACC1_BLK: USP_REEBOW_3DAP_BLK {
         ace_logistics_wirecutter_hasWirecutter = 1;
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,50);
     };
     class USP_REEBOW_3DAP_ACC2_BLK: USP_REEBOW_3DAP_BLK {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,50);
     };
     class USP_REEBOW_3DAP_ACC3_BLK: USP_REEBOW_3DAP_BLK {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,50);
     };
     class USP_REEBOW_3DAP_ACC4_BLK: USP_REEBOW_3DAP_BLK {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,50);
     };
     class USP_REEBOW_3DAP_ACC5_BLK: USP_REEBOW_3DAP_BLK {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,50);
     };
     class USP_REEBOW_3DAP_ACC6_BLK: USP_REEBOW_3DAP_BLK {
         ace_logistics_wirecutter_hasWirecutter = 1;
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,50);
     };
     class USP_REEBOW_3DAP_ACC7_BLK: USP_REEBOW_3DAP_BLK {
         ace_logistics_wirecutter_hasWirecutter = 1;
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,50);
     };
     class USP_REEBOW_3DAP_ACC8_BLK: USP_REEBOW_3DAP_BLK {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,50);
     };
     class USP_REEBOW_3DAP_ACC9_BLK: USP_REEBOW_3DAP_BLK {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,50);
     };
     class USP_REEBOW_3DAP_ACC10_BLK: USP_REEBOW_3DAP_BLK {
         ace_logistics_wirecutter_hasWirecutter = 1;
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,50);
     };
 
     class USP_PACK_BREACHER: Bag_Base {
@@ -61,141 +63,127 @@ class CfgVehicles {
         ace_logistics_wirecutter_hasWirecutter = 1;
     };
 
+    //Gleiche Werte wie Vanilla Rucksack mit dem identischem Modell
     class USP_TACTICAL_PACK: Bag_Base {
-        maximumLoad = 240; //Same a vanilla bag with the same model
-        mass = 80;
+        BACKPACK_BALANCE(240,40);
     };
     class USP_TACTICAL_PACK_CCT: Bag_Base {
-        maximumLoad = 240;
-        mass = 80;
+        BACKPACK_BALANCE(240,40);
     };
     class USP_TACTICAL_PACK_CCT2: Bag_Base {
-        maximumLoad = 240;
-        mass = 80;
+        BACKPACK_BALANCE(240,40);
     };
     class USP_TACTICAL_PACK_CCT3: Bag_Base {
-        maximumLoad = 240;
-        mass = 80;
+        BACKPACK_BALANCE(240,40);
     };
     class USP_TACTICAL_PACK_CCT4: Bag_Base {
-        maximumLoad = 240;
-        mass = 80;
+        BACKPACK_BALANCE(240,40);
     };
     class USP_TACTICAL_PACK_CCT5: Bag_Base {
-        maximumLoad = 240;
-        mass = 80;
+        BACKPACK_BALANCE(240,40);
     };
     class USP_TACTICAL_PACK_CCT6: Bag_Base {
-        maximumLoad = 240;
-        mass = 80;
+        BACKPACK_BALANCE(240,40);
     };
     class USP_TACTICAL_PACK_CCT7: Bag_Base {
-        maximumLoad = 240;
-        mass = 80;
+        BACKPACK_BALANCE(240,40);
     };
     class USP_TACTICAL_PACK_CCT8: Bag_Base {
-        maximumLoad = 240;
-        mass = 80;
+        BACKPACK_BALANCE(240,40);
     };
     class USP_TACTICAL_PACK_PO1: Bag_Base {
-        maximumLoad = 240;
-        mass = 80;
+        BACKPACK_BALANCE(240,40);
     };
     class USP_TACTICAL_PACK_PO2: Bag_Base {
-        maximumLoad = 240;
-        mass = 80;
+        BACKPACK_BALANCE(240,40);
     };
     class USP_TACTICAL_PACK_PO3: Bag_Base {
-        maximumLoad = 240;
-        mass = 80;
+        BACKPACK_BALANCE(240,40);
     };
     class USP_TACTICAL_PACK_PO4: Bag_Base {
-        maximumLoad = 240;
-        mass = 80;
+        BACKPACK_BALANCE(240,40);
     };
     class USP_TACTICAL_PACK_PO5: Bag_Base {
-        maximumLoad = 240;
-        mass = 80;
+        BACKPACK_BALANCE(240,40);
     };
     class USP_TACTICAL_PACK_PO6: Bag_Base {
-        maximumLoad = 240;
-        mass = 80;
+        BACKPACK_BALANCE(240,40);
     };
     class USP_TACTICAL_PACK_PO7: Bag_Base {
-        maximumLoad = 240;
-        mass = 80;
+        BACKPACK_BALANCE(240,40);
     };
 
+    //Gleiche Werte wie Rucksack aus CUP mit dem identischem Modell
     class USP_PATROL_PACK: Bag_Base {
-        maximumLoad = 280; //Same a cup bag with the same model
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CB: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CB_CS: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CB_CS_FH: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CB_CS_FH_RP: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CB_CS_FH_RP_ZT: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CB_CS_FH_ZT: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CB_CS_RP: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CB_CS_RP_ZT: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CB_CS_ZT: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CB_FH: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CB_FH_RP: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CB_FH_RP_ZT: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CB_FH_ZT: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CB_RP: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CB_RP_ZT: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CB_ZT: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CS: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CS_FH: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CS_FH_ZT: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_CS_ZT: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_FH: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_FH_ZT: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
     class USP_PATROL_PACK_ZT: Bag_Base {
-        maximumLoad = 280;
+        BACKPACK_BALANCE(280,40);
     };
 };
--- addons/compat_usp/CfgWeapons.hpp
+++ addons/compat_usp/CfgWeapons.hpp
@@ -1,5 +1,11 @@
 class CfgWeapons {
 
     #include "configs/Vests.hpp"
-    #include "configs/Helmets.hpp"    
+    #include "configs/Helmets.hpp"
+
+    class NVGoggles;
+    class USP_NSEAS: NVGoggles {
+        //Wenn per ACE Action entfernt wird, ist es weg statt im Inventar -> verstecken
+        scope = 0;
+    };
 };
