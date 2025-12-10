a6480926f0ad2f943398dc5f4687345d2664f6e1
Andx <andx667@protonmail.com>
2025-12-10 23:26:46 +0100

    add macros to afrf

--- addons/compat_rhs/afrf_equipment_balance_compat/CfgVehicles.hpp
+++ addons/compat_rhs/afrf_equipment_balance_compat/CfgVehicles.hpp
@@ -2,51 +2,40 @@ class CfgVehicles {
     class B_AssaultPack_Base;
     class B_Carryall_Base;
     class rhs_assault_umbts: B_AssaultPack_Base {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhs_rk_sht_30_olive: rhs_assault_umbts {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhs_rpg: B_AssaultPack_Base {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhs_sidor: B_AssaultPack_Base {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhs_medic_bag: B_AssaultPack_Base {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhs_rd54: B_AssaultPack_Base {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhs_rd54_flora1;
     class rhs_rd54_vest_flora1: rhs_rd54_flora1 {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhs_rd54_flora2;
     class rhs_rd54_vest_flora2: rhs_rd54_flora2 {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
 
     class rhs_rpg_2;
     class rhs_rpg_6b3: rhs_rpg_2 {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhs_r148: B_AssaultPack_Base {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
     class rhs_tortila_grey: B_Carryall_Base {
-        maximumLoad = 440;
-        mass = 25;
+        BACKPACK_BALANCE(440,25);
     };
 };
--- addons/compat_rhs/usaf_equipment_balance_compat/script_component.hpp
+++ addons/compat_rhs/usaf_equipment_balance_compat/script_component.hpp
@@ -2,3 +2,57 @@
 #define SUBCOMPONENT_BEAUTIFIED USAF
 
 #include "..\script_component.hpp"
+
+// ACE Hearing protection macro
+#define ACE_HEARING\
+    ace_hearing_protection = 1; \
+    ace_hearing_lowerVolume = 0.0
+
+// Headgear macros
+#define HEADGEAR_NO_PROTECTION(massValue) \
+    ACE_HEARING;\
+    mass = massValue; \
+    class HitpointsProtectionInfo { \
+        class Head { \
+            hitpointName = "HitHead"; \
+            armor = 0; \
+            passThrough = 1; \
+        }; \
+        class Face { \
+            hitpointName = "HitFace"; \
+            armor = 0; \
+            passThrough = 1; \
+        }; \
+    }
+
+#define HEADGEAR_ARMOR(massValue,headArmor,faceArmor) \
+    ACE_HEARING;\
+    mass = massValue; \
+    class HitpointsProtectionInfo { \
+        class Head { \
+            hitpointName = "HitHead"; \
+            Armor = headArmor; \
+            passThrough = 0.5; \
+        }; \
+        class Face { \
+            hitpointName = "HitFace"; \
+            armor = faceArmor; \
+            passThrough = 0.5; \
+        }; \
+    }
+
+#define HEADGEAR_ARMOR_FACE_EXPOSED(massValue,headArmor) \
+    ACE_HEARING;\
+    mass = massValue; \
+    class HitpointsProtectionInfo { \
+        class Head { \
+            hitpointName = "HitHead"; \
+            Armor = headArmor; \
+            passThrough = 0.5; \
+        }; \
+        class Face { \
+            hitpointName = "HitFace"; \
+            armor = 0; \
+            passThrough = 1; \
+        }; \
+    }
