da5ae8e8717473976a006ae3f2ba4e7de53261d6
Andx <andx667@protonmail.com>
2025-12-10 22:33:01 +0100

    refactor backpacks

--- addons/compat_rhs/script_component.hpp
+++ addons/compat_rhs/script_component.hpp
@@ -3,3 +3,8 @@
 
 #include "\z\ttt\addons\main\script_mod.hpp"
 #include "\z\ttt\addons\main\script_macros.hpp"
+
+// Backpack balance macro
+#define BACKPACK_BALANCE(maxLoad,massValue) \
+    maximumLoad = maxLoad; \
+    mass = massValue
