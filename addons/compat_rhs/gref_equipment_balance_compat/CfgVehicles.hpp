ea0e8dfb013fe1122cb254f51f78c9a00696bffb
Andx <andx667@protonmail.com>
2025-12-15 14:10:26 +0100

    update backpacks gref

--- addons/compat_rhs/gref_equipment_balance_compat/CfgVehicles.hpp
+++ addons/compat_rhs/gref_equipment_balance_compat/CfgVehicles.hpp
@@ -9,6 +9,24 @@ class CfgVehicles {
     class rhsusf_falconii;
     class B_Carryall_oli;
     //PACKS
+
+    class rhsgref_ins_pack: B_FieldPack_khk {
+        BACKPACK_BALANCE(320,320);
+    };
+    class rhsgref_tortila_specter: B_Carryall_Base {
+        BACKPACK_BALANCE(320,60);
+    };
+    class rhsgref_hidf_alicepack: Bag_Base {
+        BACKPACK_BALANCE(320,60);
+    };
+    class rhsgref_wdl_alicepack: rhsgref_hidf_alicepack {
+        BACKPACK_BALANCE(320,60);
+    };
+    class rhsgref_ttsko_alicepack: rhsgref_hidf_alicepack {
+        BACKPACK_BALANCE(320,60);
+    };
+
+    //Hidden Items
     class rhsgref_cdf_backpack_mg: B_Carryall_oli {
         BACKPACK_BALANCE(440,25);
     };
@@ -39,25 +57,10 @@ class CfgVehicles {
     class rhsgref_nat_backpack_explosives: B_TacticalPack_oli {
         BACKPACK_BALANCE(440,25);
     };
-    class rhsgref_ins_pack: B_FieldPack_khk {
-        BACKPACK_BALANCE(440,25);
-    };
     class rhsgref_ins_pack_GR: B_FieldPack_cbr {
         BACKPACK_BALANCE(440,25);
     };
     class rhsgref_ins_packMG: B_TacticalPack_blk {
-        BACKPACK_BALANCE(440,25);
-    };
-    class rhsgref_tortila_specter: B_Carryall_Base {
-        BACKPACK_BALANCE(440,25);
-    };
-    class rhsgref_hidf_alicepack: Bag_Base {
-        BACKPACK_BALANCE(440,25);
-    };
-    class rhsgref_wdl_alicepack: rhsgref_hidf_alicepack {
-        BACKPACK_BALANCE(440,25);
-    };
-    class rhsgref_ttsko_alicepack: rhsgref_hidf_alicepack {
         BACKPACK_BALANCE(440,25);
     };
 };
