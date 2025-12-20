4a620632a1b151f3efd5b82e953cee34a86a3185
Andx <andx667@protonmail.com>
2025-12-20 21:42:01 +0100

    init

--- addons/compat_3cb_vehicles/CfgVehicles.hpp
+++ addons/compat_3cb_vehicles/CfgVehicles.hpp
@@ -1,10 +1,21 @@
 //https://ace3.acemod.org/wiki/framework/interactionmenu-framework#22-vehicle-interactions-based-on-animations
 
 class CfgVehicles {
+
+    class UK3CB_BAF_LandRover_Base;
+    class UK3CB_BAF_LandRover_Panama_Base: UK3CB_BAF_LandRover_Base {
+        mineDetectorRange = 25; //Vanilla ED-1D hat 50m Reichweite
+        class components {
+            class MineDetectorDisplay {
+                range = 25;
+            };
+        };
+    };
+
     class UK3CB_BAF_LandRover_WMIK_Base;
     class UK3CB_BAF_LandRover_WMIK_Milan_Base: UK3CB_BAF_LandRover_WMIK_Base {
         class ace_interaction_anims {
-            class Gear_Hide { 
+            class Gear_Hide {
                 positions[] = {{1, 0.7, -0.4},{1.1, -1.1, -1.1}, {-1.25, -1.1, -1.1}};
                 items[] = {"ACE_EntrenchingTool", "Land_CanisterFuel_F", "Land_CanisterFuel_F", "UK3CB_BAF_B_Bergen_TAN_Rifleman_A", "UK3CB_BAF_B_Bergen_TAN_Rifleman_B", "UK3CB_BAF_B_Bergen_TAN_SL_A"};
                 text = "Ausrüstung abnehmen";
@@ -14,7 +25,7 @@ class CfgVehicles {
 
     class UK3CB_BAF_LandRover_WMIK_GMG_Base: UK3CB_BAF_LandRover_WMIK_Base {
         class ace_interaction_anims {
-            class Gear_Hide { 
+            class Gear_Hide {
                 positions[] = {{-1.25, -1.6, -1.1},{1.1, -1.6, -1.1}, {1.1, 0, -0.7}}; //ToDo add the positione of the other two Backpacks
                 items[] = {"ACE_EntrenchingTool", "Land_CanisterFuel_F", "Land_CanisterFuel_F", "UK3CB_BAF_B_Bergen_TAN_Rifleman_A", "UK3CB_BAF_B_Bergen_TAN_Rifleman_B", "UK3CB_BAF_B_Bergen_TAN_SL_A"};
                 text = "Ausrüstung abnehmen";
@@ -24,7 +35,7 @@ class CfgVehicles {
 
     class UK3CB_BAF_LandRover_WMIK_HMG_Base: UK3CB_BAF_LandRover_WMIK_Base {
         class ace_interaction_anims {
-            class Gear_Hide { 
+            class Gear_Hide {
                 positions[] = {{-1.25, -1.6, -1.1},{1.1, -1.6, -1.1}, {1.1, 0, -0.7}};
                 items[] = {"ACE_EntrenchingTool", "Land_CanisterFuel_F", "Land_CanisterFuel_F", "UK3CB_BAF_B_Bergen_TAN_Rifleman_A", "UK3CB_BAF_B_Bergen_TAN_Rifleman_B", "UK3CB_BAF_B_Bergen_TAN_SL_A"};
                 text = "Ausrüstung abnehmen";
