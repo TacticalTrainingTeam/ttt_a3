a170c635e922155f241685181594af1fb7346591
Andx <andx667@protonmail.com>
2026-01-27 12:34:52 +0100

    update

--- addons/common/configs/ACE_Medical_Treatment_Actions.hpp
+++ addons/common/configs/ACE_Medical_Treatment_Actions.hpp
@@ -1,8 +1,8 @@
 class ACE_medical_treatment_actions {
     class PersonalAidKit;
     class TTT_PersonalAidKit: PersonalAidKit {
-        displayName = CSTRING(PersonalAidKit_DisplayName);
-        displayNameProgress = CSTRING(PersonalAidKit_Progress);
+        displayName = CSTRING(PersonalAidKit_Action_DisplayName);
+        displayNameProgress = CSTRING(PersonalAidKit_Action_Progress);
         condition = "true";
         items[] = {"TTT_personalAidKit"};
         treatmentTime = 60;
