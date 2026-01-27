c8cff194903ec316fcf006993dc766d33e43eaa9
Andx <andx667@protonmail.com>
2026-01-27 10:56:34 +0100

    improve

--- addons/common/configs/ACE_Medical_Treatment_Actions.hpp
+++ addons/common/configs/ACE_Medical_Treatment_Actions.hpp
@@ -1,9 +1,10 @@
 class ACE_medical_treatment_actions {
     class PersonalAidKit;
     class TTT_PersonalAidKit: PersonalAidKit {
-        displayName = "TTT PAK Nutzen";//ToDo Localize
-        displayNameProgress = "TTT PAK wird benutzt";//ToDo Localize
-        condition = "true";//QUOTE(_patient call EFUNC(medical_status,isInStableCondition));
+        displayName = CSTRING(PersonalAidKit_DisplayName);
+        displayNameProgress = CSTRING(PersonalAidKit_Progress);
+        condition = "true";
         items[] = {"TTT_personalAidKit"};
+        treatmentTime = 60;
     };
 };
