1dc1c84a8164a650417918cb3ed6f036fcd05d0a
Andx <andx667@protonmail.com>
2026-09-07 18:31:15 +0200

    Update FPV drone configurations and descriptions; replace old IDs with new Crocus variants

--- addons/compat_fpv/CfgVehicles.hpp
+++ addons/compat_fpv/CfgVehicles.hpp
@@ -4,7 +4,7 @@ class CfgVehicles {
         class ACE_SelfActions {
             class ACE_Equipment {
                 class GVAR(assemble) {
-                    displayName = "Assemble";
+                    displayName = CSTRING(assemble);
                     condition = QUOTE([_player] call FUNC(action_canAssemble));
                     insertChildren = QUOTE([_player] call FUNC(action_insertChildren));
                     statement = "";
@@ -13,43 +13,56 @@ class CfgVehicles {
         };
     };
 
-    class Air;
-    class Helicopter: Air {
+    //FPV
+    class Helicopter_Base_F;
+    class drone_base_F: Helicopter_Base_F {
         class ACE_Actions;
     };
-    class Helicopter_Base_F: Helicopter {
-        class ACE_Actions: ACE_Actions {
+    class ARMAFPV_Crocus_AT_Base: drone_base_F {
+        class ACE_Actions {
             class ACE_MainActions;
         };
     };
-
-    //FPV
-    class ua_drone_base_F: Helicopter_Base_F {
+    class B_Crocus_AT: ARMAFPV_Crocus_AT_Base {
         class ACE_Actions: ACE_Actions {
             class ACE_MainActions: ACE_MainActions {
                 class GVAR(pack) {
-                    displayName = "Pack";
+                    displayName = CSTRING(pack);
                     distance = 4;
                     condition = QUOTE(alive _target);
                     statement = QUOTE([ARR_2(_player,_target)] call FUNC(pack));
                 };
             };
         };
+
+        GVAR(case) = QCLASS(B_Crocus_AT_CASE);
     };
 
-    class B_UAFPV_IED_AP: ua_drone_base_F {
-        GVAR(case) = QCLASS(B_UAFPV_IED_AP_CASE);
+    class B_Crocus_AT_TI: B_Crocus_AT {
+        GVAR(case) = QCLASS(B_Crocus_AT_TI_CASE);
     };
 
-    class B_UAFPV_OG7V_AP: ua_drone_base_F {
-        GVAR(case) = QCLASS(B_UAFPV_OG7V_AP_CASE);
+    class ARMAFPV_Crocus_AP_Base: drone_base_F {
+        class ACE_Actions: ACE_Actions {
+            class ACE_MainActions;
+        };
+    };
+    class B_Crocus_AP: ARMAFPV_Crocus_AP_Base {
+        class ACE_Actions: ACE_Actions {
+            class ACE_MainActions: ACE_MainActions {
+                class GVAR(pack) {
+                    displayName = CSTRING(pack);
+                    distance = 4;
+                    condition = QUOTE(alive _target);
+                    statement = QUOTE([ARR_2(_player,_target)] call FUNC(pack));
+                };
+            };
+        };
+
+        GVAR(case) = QCLASS(B_Crocus_AP_CASE);
     };
 
-    class B_UAFPV_PG7VL_AT: ua_drone_base_F {
-        GVAR(case) = QCLASS(B_UAFPV_PG7VL_AT_CASE);
-    };
-
-    class B_UAFPV_RKG_AP: ua_drone_base_F {
-        GVAR(case) = QCLASS(B_UAFPV_RKG_AP_CASE);
+    class B_Crocus_AP_TI: B_Crocus_AP {
+        GVAR(case) = QCLASS(B_Crocus_AP_TI_CASE);
     };
 };
